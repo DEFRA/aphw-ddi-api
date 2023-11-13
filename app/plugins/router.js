e298ff635fd04b2a5eb6b59dca6c458b49f734d4
Shaun Fitzsimons <shaun.r.fitzsimons@gmail.com>
2023-11-13 10:44:22 +0000

    Add faker to package.json (#7)
    * Add faker to package.json
    
    * Test remove AAD
    
    * Revert "Test remove AAD"
    
    This reverts commit dc0f2fbe61c721dfd6aa434a1ff897a2dbbfae10.
    
    * Add check db connection
    
    * Remove unrequired data from breed seed data
    
    * Test disable backlog routes
    
    * Reenable import
    
    * Re-enable backlog
    
    * Re-enable faker route
    
    * Remove mock backlog routes
    
    * Remove faker-js

--- app/plugins/router.js
+++ app/plugins/router.js
@@ -5,7 +5,6 @@ const routes = [].concat(
   require('../routes/healthy'),
   require('../routes/healthz'),
   require('../routes/import-access-db'),
-  require('../routes/fake-backlog'),
   require('../routes/process-backlog'),
   require('../routes/person'),
   require('../routes/search-index')
