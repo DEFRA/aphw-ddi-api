fd87659892eb98754adabd747fc261f43b40a8e3
sfitz42 <shaun.r.fitzsimons@gmail.com>
2023-11-24 15:52:24 +0000

    Update index_number to virtual column

--- changelog/sql/changelog-1.25/index_number_generate.sql
+++ changelog/sql/changelog-1.25/index_number_generate.sql
@@ -1,3 +1,3 @@
 ALTER TABLE ${SCHEMA_NAME}.dog 
     ADD COLUMN index_number VARCHAR(20) 
-    GENERATED ALWAYS AS ('ED' || index_number_base::TEXT) STORED;
+    GENERATED ALWAYS AS ('ED' || index_number_base::TEXT);
