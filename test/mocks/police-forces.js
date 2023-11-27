dbc7783ddbc60da39a8343a290d62c9f11bd3946
Shaun Fitzsimons <shaun.r.fitzsimons@gmail.com>
2023-11-27 17:01:21 +0000

    270468 - Add get police forces endpoint (#16)
    * Add get police forces endpoint
    
    * Add error logging to police force
    
    * Change error log output
    
    * Change error log output
    
    * Add police force id

---
+++ test/mocks/police-forces.js
@@ -0,0 +1,18 @@
+const forces = [
+  {
+    id: 1,
+    name: 'Northern Constabulary'
+  },
+  {
+    id: 2,
+    name: 'Southern Constabulary'
+  },
+  {
+    id: 3,
+    name: 'Eastern Constabulary'
+  }
+]
+
+module.exports = {
+  forces
+}
