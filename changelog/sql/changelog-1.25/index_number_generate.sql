baa12f0acd49efcd9dc27cc00a32a00b74a74c24
sfitz42 <shaun.r.fitzsimons@gmail.com>
2023-11-27 07:53:52 +0000

    Switch from virtual column to trigger

--- changelog/sql/changelog-1.25/index_number_generate.sql
+++ changelog/sql/changelog-1.25/index_number_generate.sql
@@ -1,3 +1,11 @@
-ALTER TABLE ${SCHEMA_NAME}.dog 
-    ADD COLUMN index_number VARCHAR(20) 
-    GENERATED ALWAYS AS ('ED' || index_number_base::TEXT);
+CREATE OR REPLACE FUNCTION generate_index_number()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.index_number := 'ED' || NEW.index_number_base::TEXT;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER cdo_create_trigger
+BEFORE INSERT OR UPDATE ON dog
+FOR EACH ROW EXECUTE FUNCTION generate_index_number();
