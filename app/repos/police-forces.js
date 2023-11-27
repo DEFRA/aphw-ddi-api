8fac32ec72f86533c5ef652129dcf94eb730eff0
sfitz42 <shaun.r.fitzsimons@gmail.com>
2023-11-27 13:10:50 +0000

    Change error log output

--- app/repos/police-forces.js
+++ app/repos/police-forces.js
@@ -2,9 +2,11 @@ const sequelize = require('../config/db')
 
 const getForces = async () => {
   try {
-    return sequelize.models.police_force.findAll({
+    const forces = await sequelize.models.police_force.findAll({
       attributes: ['name']
     })
+
+    return forces
   } catch (e) {
     console.log(`Error retrieving police forces: ${e}`)
     throw e
