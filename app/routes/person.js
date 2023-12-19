8f986a21f4b10b65c4e404af30765b378658cd14
Shaun Fitzsimons <shaun.r.fitzsimons@gmail.com>
2023-12-19 13:18:53 +0000

    275839 - Owner update endpoint (#34)
    * Add person update
    
    * Fix linting
    
    * Increase test coverage

--- app/routes/person.js
+++ app/routes/person.js
@@ -1,6 +1,7 @@
 const Joi = require('joi')
-const { getPersonByReference, getPersonAndDogsByReference } = require('../repos/people')
+const { getPersonByReference, getPersonAndDogsByReference, updatePerson } = require('../repos/people')
 const { personDto, personAndDogsDto } = require('../dto/person')
+const { schema: updateSchema } = require('../schema/person/update')
 
 module.exports = [{
   method: 'GET',
@@ -26,6 +27,34 @@ module.exports = [{
       const result = includeDogs ? personAndDogsDto(person) : personDto(person)
 
       return h.response(result).code(200)
+    }
+  }
+}, {
+  method: 'PUT',
+  path: '/person',
+  options: {
+    validate: {
+      payload: updateSchema,
+      failAction: (request, h, error) => {
+        return h.response().code(400).takeover()
+      }
+    },
+    handler: async (request, h) => {
+      const person = request.payload
+
+      try {
+        const updated = await updatePerson(person)
+
+        return h.response(personDto(updated)).code(200)
+      } catch (err) {
+        console.error(`Error updating person: ${err}`)
+
+        if (err.type === 'NOT_FOUND') {
+          return h.response().code(400)
+        }
+
+        throw err
+      }
     }
   }
 }]
