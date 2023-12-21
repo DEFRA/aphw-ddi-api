a06659f23cf28ee1b889829db229e46c6bd0977f
work <jeremy.barnsley@esynergy.co.uk>
2023-12-21 11:11:13 +0000

    Commented out local config

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,5 @@
 jest.setTimeout(30000)
-
+/*
 // Uncomment the following lines if you need to run a single test with :
 // npm run test <path>/<test-file>.test.js
 //
@@ -12,3 +12,4 @@ process.env.DDI_API_BASE_URL = 'http://localhost/api'
 process.env.OS_PLACES_API_BASE_URL = 'http://localhost/os-places'
 process.env.OS_PLACES_API_KEY = 'some-api-key'
 process.env.POLICE_API_BASE_URL = 'http://localhost/police'
+*/
