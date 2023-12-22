b406a9c7188cc06bbabfa176f5c02f2a0897a96a
Jez Barnsley <114290619+jbarnsley10@users.noreply.github.com>
2023-12-22 14:38:02 +0000

    286058 address fix (#37)
    * 286058 address fix
    
    * Bumped version
    
    * Bumped version
    
    ---------
    
    Co-authored-by: work <jeremy.barnsley@esynergy.co.uk>

--- app/register/search-basic.js
+++ app/register/search-basic.js
@@ -1,6 +1,6 @@
 const sequelize = require('../config/db')
 const { Op } = require('sequelize')
-const { buildAddressString } = require('../lib/address-helper')
+const { buildAddressForSearchResults } = require('../lib/address-helper')
 
 const search = async (type, terms) => {
   if (terms === null || terms === undefined) {
@@ -61,7 +61,7 @@ const groupOwners = results => {
       personReference: value[0].personReference,
       firstName: value[0].firstName,
       lastName: value[0].lastName,
-      address: buildAddressString(value[0].address),
+      address: buildAddressForSearchResults(value[0].address),
       dogs: value.map(y => ({
         dogId: y.dogId,
         dogIndex: y.dogIndex,
