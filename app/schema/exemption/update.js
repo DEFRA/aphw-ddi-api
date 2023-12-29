855598eed1ae5099c2783dc3284d09a7a04f36fe
sfitz42 <shaun.r.fitzsimons@gmail.com>
2023-12-29 10:25:14 +0000

    Add order specific update

--- app/schema/exemption/update.js
+++ app/schema/exemption/update.js
@@ -1,4 +1,5 @@
 const Joi = require('joi')
+const { getCdo } = require('../../repos/cdo')
 
 const exemption = Joi.object({
   indexNumber: Joi.string().required(),
@@ -18,6 +19,33 @@ const exemption = Joi.object({
   }).optional()
 })
 
+const orderSpecific = Joi.object({
+  exemptionOrder: Joi.string().optional(),
+  microchipDeadline: Joi.date().iso().optional(),
+  typedByDlo: Joi.date().iso().optional(),
+  withdrawn: Joi.date().iso().optional()
+})
+
+const validatePayload = async (payload) => {
+  let schema = exemption
+
+  const cdo = await getCdo(payload.indexNumber)
+
+  const order = cdo?.registration.exemption_order.exemption_order
+
+  if (order === '2023') {
+    schema = schema.concat(orderSpecific)
+  }
+
+  const { value, error } = schema.validate(payload)
+
+  if (error) {
+    throw error
+  }
+
+  return value
+}
+
 module.exports = {
-  exemption
+  validatePayload
 }
