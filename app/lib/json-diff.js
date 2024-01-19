e9fc98f236bf0ca757a8c92b4858ebfa77bcdd9a
work <jeremy.barnsley@esynergy.co.uk>
2024-01-19 08:16:24 +0000

    Added json-diff test

--- app/lib/json-diff.js
+++ app/lib/json-diff.js
@@ -3,13 +3,13 @@ const { deepClone } = require('../lib/deep-clone')
 const isoDateEnding = 'T00:00:00.000Z'
 
 const jsonDiff = (obj1, obj2) => {
-  return diff(deepClone(obj1), deepClone(obj2))
+  return diff(deepClone(obj1), deepClone(obj2)) ?? {}
 }
 
 const diff = (obj1, obj2) => {
   const result = {}
 
-  if (Object.is(obj1, obj2)) return undefined
+  if (Object.is(obj1, obj2)) return result
 
   if (!obj2 || typeof obj2 !== 'object') return obj2
 
