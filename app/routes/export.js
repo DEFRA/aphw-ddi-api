8307527e1c12dee8abcd8c99f241a7edb68e7941
work <jeremy.barnsley@esynergy.co.uk>
2024-02-20 12:47:53 +0000

    Temp route to manually trigger overnight jobs

--- app/routes/export.js
+++ app/routes/export.js
@@ -2,8 +2,9 @@ const { getCallingUser } = require('../auth/get-user')
 const { readExportFile } = require('../export/read-export-file')
 const { EXPORT } = require('../constants/event/events')
 const { sendEventToAudit } = require('../messaging/send-audit')
+const { runOvernightJobs } = require('../repos/regular-jobs')
 
-module.exports = {
+module.exports = [{
   method: 'GET',
   path: '/export',
   handler: async (request, h) => {
@@ -15,4 +16,13 @@ module.exports = {
       csv
     }).code(200)
   }
-}
+},
+{
+  method: 'GET',
+  path: '/export-trigger-overnight',
+  handler: async (request, h) => {
+    await runOvernightJobs()
+
+    return h.response().code(200)
+  }
+}]
