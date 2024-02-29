2d8495c48d508a96b00883424936d38e2d1a3c0a
Chris Cole <56303993+whitewaterdesign@users.noreply.github.com>
2024-02-29 15:36:21 +0000

    test: Countries contract test api (#84)
    * test: Countries contract test api
    
    * build: minor version bump to 0.65.1
    
    * build: add postgres to docker build test step
    
    ---------
    
    Co-authored-by: Chris Cole <whitewaterdesign>

---
+++ test/contract/aphw-ddi-api.test.js
@@ -0,0 +1,55 @@
+const config = require('../../app/config')
+require('../../app/config/db')
+const dbHelper = require('../db-helper')
+
+describe('Pact Verification', () => {
+  const { Verifier } = require('@pact-foundation/pact')
+
+  let createServer
+  let server
+
+  async function createTestData () {
+    await dbHelper.truncate()
+  }
+
+  beforeAll(async () => {
+    await createTestData()
+    createServer = require('../../app/server')
+  })
+
+  beforeEach(async () => {
+    server = await createServer()
+    await server.start()
+  })
+
+  test('validates the expectations of aphw-ddi-api', async () => {
+    const opts = {
+      providerBaseUrl: `http://localhost:${config.port}`,
+      provider: 'aphw-ddi-api',
+      consumerVersionTags: ['main', 'dev', 'test', 'preprod', 'prod'],
+      pactBrokerUrl: process.env.PACT_BROKER_URL,
+      pactBrokerUsername: process.env.PACT_BROKER_USERNAME,
+      pactBrokerPassword: process.env.PACT_BROKER_PASSWORD,
+      stateHandlers: {
+        'countries exist': async () => {
+          await dbHelper.createCountryRecords([
+            { id: 1, country: 'England' },
+            { id: 2, country: 'Scotland' },
+            { id: 3, country: 'Wales' }
+          ])
+          return 'Countries added to db'
+        }
+      }
+    }
+
+    await new Verifier(opts).verifyProvider()
+  })
+
+  afterEach(async () => {
+    await server.stop()
+  })
+
+  afterAll(async () => {
+    await dbHelper.close()
+  })
+})
