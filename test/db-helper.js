a684e58430477175966c75ae577524acae756cb3
Chris Cole <whitewaterdesign>
2024-03-07 12:15:07 +0000

    feat: 297910 - Add new dbHelper methods for add person

--- test/db-helper.js
+++ test/db-helper.js
@@ -1,8 +1,21 @@
 require('../app/data')
 const sequelize = require('../app/config/db')
+const { getCountry } = require('../app/lookups')
 
 const truncate = async () => {
-  await sequelize.models.country.destroy({ where: {}, truncate: false })
+  await sequelize.models.search_index.truncate()
+  await sequelize.models.comment.truncate()
+  await sequelize.models.dog_microchip.truncate()
+  await sequelize.models.microchip.destroy({ where: {}, truncate: false })
+  await sequelize.models.registration.destroy({ where: {}, truncate: false })
+  await sequelize.models.insurance.truncate()
+  await sequelize.models.person_address.truncate()
+  await sequelize.models.address.destroy({ where: {}, truncate: false })
+  await sequelize.models.person_contact.destroy({ where: {}, truncate: false })
+  await sequelize.models.contact.destroy({ where: {}, truncate: false })
+  await sequelize.models.registered_person.truncate()
+  await sequelize.models.person.destroy({ where: {}, truncate: false })
+  await sequelize.models.dog.destroy({ where: {}, truncate: false })
 }
 
 /**
@@ -13,6 +26,38 @@ const createCountryRecords = async (countries) => {
   await sequelize.models.country.bulkCreate(countries)
 }
 
+/**
+ * @param {Person} [personRequest]
+ * @returns {Promise<void>}
+ */
+const addPerson = async (personRequest) => {
+  const defaultPerson = {
+    first_name: 'Homer',
+    last_name: 'Simpson',
+    person_reference: 'P-D9E1-22AD',
+    birth_date: '1998-05-10'
+  }
+
+  const country = await getCountry('Wales')
+
+  const defaultAddress = {
+    address_line_1: '1 Anywhere St',
+    address_line_2: 'Anywhere Estate',
+    town: 'Pontypridd',
+    postcode: 'CF15 7SU',
+    country_id: country.id
+  }
+
+  const person = await sequelize.models.person.create(defaultPerson)
+
+  const address = await sequelize.models.address.create(defaultAddress)
+
+  await sequelize.models.person_address.create({
+    person_id: person.id,
+    address_id: address.id
+  })
+}
+
 const close = async () => {
   await sequelize.close()
 }
@@ -20,5 +65,6 @@ const close = async () => {
 module.exports = {
   close,
   truncate,
-  createCountryRecords
+  createCountryRecords,
+  addPerson
 }
