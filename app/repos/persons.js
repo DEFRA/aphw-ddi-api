a274524f105a32991c0eb666e0f5abfd298f97f2
Chris Cole <56303993+whitewaterdesign@users.noreply.github.com>
2024-03-11 15:03:50 +0000

    Feat/297910  duplicate owners get persons (#87)
    * feat: 297910 - Schema for GET /persons
    
    * feat: 297910 - Add response schema for GET /persons
    
    * feat: 297910 - GET /persons scaffolding
    
    * feat: 297910 - getPersons repo scaffolding
    
    * feat: 297910 - Update GET /persons response schema with contacts
    
    * feat: 297910 - GET /persons mapping
    
    * feat: 297910 - getPersons repo where call
    
    * feat: 297910 - getPersons repo where call cleanup
    
    * feat: 297910 - getPersons repo col ordering
    
    * feat: 297910 - Transaction on createPeople dup pk
    
    * chore: Patch version 0.65.4
    
    * feat: 297910 - Updates following PR
    
    * refactor: 297910 - Pull out person relationships
    
    * refactor: 297910 - Pull out refactor to method
    
    ---------
    
    Co-authored-by: Chris Cole <whitewaterdesign>

---
+++ app/repos/persons.js
@@ -0,0 +1,51 @@
+const sequelize = require('../config/db')
+const { personTableRelationships } = require('./relationships/person')
+/**
+ * @typedef GetPersonsFilter
+ * @property {string} [firstName]
+ * @property {string} [lastName]
+ * @property {Date} [dateOfBirth]
+ */
+const MAX_RESULTS = 20
+
+const dtoToModelMapping = {
+  firstName: 'first_name',
+  lastName: 'last_name',
+  dateOfBirth: 'birth_date'
+}
+
+/**
+ * @param {GetPersonsFilter} queryParams
+ */
+const getPersons = async (queryParams, transaction) => {
+  /**
+   * @type {{first_name?: string, last_name?: string, birth_date?: string}}
+   */
+  const where = Object.keys(dtoToModelMapping).reduce((whereObject, key) => {
+    const query = queryParams[key]
+
+    if (query) {
+      const dbColumnKey = dtoToModelMapping[key]
+      whereObject[dbColumnKey] = query
+    }
+    return whereObject
+  }, {})
+
+  try {
+    return await sequelize.models.person.findAll({
+      where,
+      include: personTableRelationships(sequelize),
+      order: [[sequelize.col('addresses.address.id'), 'DESC']],
+      limit: MAX_RESULTS,
+      subQuery: false,
+      transaction
+    })
+  } catch (err) {
+    console.error(`Error getting people: ${err}`)
+    throw err
+  }
+}
+
+module.exports = {
+  getPersons
+}
