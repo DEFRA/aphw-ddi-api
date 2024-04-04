7ac175121c709dbf8475f566f3f50ba94f8777ca
work <jeremy.barnsley@esynergy.co.uk>
2024-04-04 15:50:59 +0100

    Removed comment

--- app/routes/process-comments.js
+++ app/routes/process-comments.js
@@ -1,13 +1,5 @@
 const { processComments } = require('../import/access/backlog/process-comments')
 
-// const buildConfig = (request) => ({
-//   maxRecords: request.query.maxRecords,
-//   includeFuzzyAlgo1: request.query.includeFuzzyAlgo1 === 'true',
-//   includeFuzzyAlgo2: request.query.includeFuzzyAlgo2 === 'true',
-//   includeSwappedNames: request.query.includeSwappedNames === 'true',
-//   validateOnly: request.query.validateOnly === 'true'
-// })
-
 module.exports = {
   method: 'GET',
   path: '/process-comments',
