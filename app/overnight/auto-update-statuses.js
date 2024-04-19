a66cd247aeba9ad2ebed6868ba2077547757a363
Jez Barnsley <114290619+jbarnsley10@users.noreply.github.com>
2024-04-19 08:19:30 +0100

    Disabled neutering and microchip overnight jobs (#109)
    * Disabled jobs
    
    * Patch bump
    
    ---------
    
    Co-authored-by: work <jeremy.barnsley@esynergy.co.uk>

--- app/overnight/auto-update-statuses.js
+++ app/overnight/auto-update-statuses.js
@@ -1,8 +1,6 @@
 const sequelize = require('../config/db')
-const { setExpiredNeuteringDeadlineToInBreach } = require('./expired-neutering-deadline')
 const { setExpiredCdosToFailed } = require('./expired-cdo')
 const { setExpiredInsuranceToBreach } = require('./expired-insurance')
-const { setExpiredMicrochipDeadlineToInBreach } = require('./expired-microchip-deadline')
 
 const autoUpdateStatuses = async () => {
   let result = ''
@@ -18,8 +16,6 @@ const autoUpdateStatuses = async () => {
     await sequelize.transaction(async (t) => {
       result = result + await setExpiredCdosToFailed(today, user, t)
       result = result + ' | ' + await setExpiredInsuranceToBreach(today, user, t)
-      result = result + ' | ' + await setExpiredNeuteringDeadlineToInBreach(today, user, t)
-      result = result + ' | ' + await setExpiredMicrochipDeadlineToInBreach(today, user, t)
     })
   } catch (e) {
     console.log(`Error auto-updating statuses: ${e}`)
