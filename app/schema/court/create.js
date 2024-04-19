0d2d80a809c126f652e53e180c3b02020220dec2
Chris Cole <whitewaterdesign>
2024-04-19 11:35:33 +0100

    refactor: 348166 - Court schema cleanup

--- app/schema/court/create.js
+++ app/schema/court/create.js
@@ -4,14 +4,6 @@ const createCourtSchema = Joi.object({
   name: Joi.string().required()
 }).required()
 
-const responseSchema = Joi.object({
-  courts: Joi.array().items(Joi.object({
-    id: Joi.number().required(),
-    name: Joi.string().required()
-  })).required()
-})
-
 module.exports = {
-  createCourtSchema,
-  responseSchema
+  createCourtSchema
 }
