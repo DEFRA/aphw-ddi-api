649ce0d6fc8c1a68036a13fc8b3945c4cd74e108
work <jeremy.barnsley@esynergy.co.uk>
2024-05-10 10:09:43 +0100

    Merge issues

--- app/constants/event/events.js
+++ app/constants/event/events.js
@@ -1,4 +1,5 @@
 module.exports = {
+  ACTIVITY: 'uk.gov.defra.ddi.event.activity',
   CREATE: 'uk.gov.defra.ddi.event.create',
   IMPORT: 'uk.gov.defra.ddi.event.import',
   IMPORT_MANUAL: 'uk.gov.defra.ddi.event.import.manual',
