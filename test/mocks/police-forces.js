cf35cc59257d1597dfcb0a61029bd1fc8ebc07c1
Chris Cole <whitewaterdesign>
2024-05-13 16:19:48 +0100

    fix: 377660 - Police force alpha sorting

--- test/mocks/police-forces.js
+++ test/mocks/police-forces.js
@@ -1,15 +1,15 @@
 const forces = [
   {
+    id: 3,
+    name: 'Eastern Constabulary'
+  },
+  {
     id: 1,
     name: 'Northern Constabulary'
   },
   {
     id: 2,
     name: 'Southern Constabulary'
-  },
-  {
-    id: 3,
-    name: 'Eastern Constabulary'
   }
 ]
 
