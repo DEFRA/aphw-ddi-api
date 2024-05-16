cbd182a6187ede501f62027688fa565a925b01e8
work <jeremy.barnsley@esynergy.co.uk>
2024-05-16 14:12:11 +0100

    File download now from Portal-side

--- app/routes/export.js
+++ app/routes/export.js
@@ -1,20 +1,15 @@
 const { getCallingUser } = require('../auth/get-user')
-const { readExportFile } = require('../export/read-export-file')
 const { EXPORT } = require('../constants/event/events')
 const { sendEventToAudit } = require('../messaging/send-audit')
 const { runExportNow } = require('../repos/regular-jobs')
 
 module.exports = [{
   method: 'GET',
-  path: '/export',
+  path: '/export-audit',
   handler: async (request, h) => {
-    const csv = await readExportFile()
-
     await sendEventToAudit(EXPORT, 'DDI Export', 'Export data', getCallingUser(request))
 
-    return h.response({
-      csv
-    }).code(200)
+    return h.response().code(200)
   }
 },
 {
