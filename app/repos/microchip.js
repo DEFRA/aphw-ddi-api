e29f39b79ed29c928e2ad13be71f47610b4cea34
Chris Cole <whitewaterdesign>
2024-05-17 15:11:39 +0100

    feat: 373491 - Prevent duplicate microchip entry when updateMicrochips

--- app/repos/microchip.js
+++ app/repos/microchip.js
@@ -1,6 +1,42 @@
 const sequelize = require('../config/db')
+const { DuplicateResourceError } = require('../errors/duplicate-record')
+const { Op } = require('sequelize')
+
+const microchipExists = async (dogId, microchipNumber, transaction) => {
+  const microchip = await sequelize.models.microchip.findOne({
+    include: [{
+      model: sequelize.models.dog_microchip,
+      as: 'dog_microchips',
+      where: {
+        dog_id: {
+          [Op.not]: dogId
+        }
+      }
+    }],
+    where: {
+      microchip_number: microchipNumber
+    },
+    transaction
+  })
+
+  return microchip?.microchip_number ?? null
+}
 
 const updateMicrochips = async (dogFromDb, payload, transaction) => {
+  const duplicateMicrochips = []
+  const microchipsToCheck = [payload.microchipNumber, payload.microchipNumber2].filter(mC => !!mC)
+
+  for (const microchipNumber of microchipsToCheck) {
+    const duplicateMicrochip = await microchipExists(dogFromDb.id, microchipNumber, transaction)
+    if (duplicateMicrochip !== null) {
+      duplicateMicrochips.push(duplicateMicrochip)
+    }
+  }
+
+  if (duplicateMicrochips.length) {
+    throw new DuplicateResourceError('The microchip number already exists', { microchipNumbers: duplicateMicrochips })
+  }
+
   await updateMicrochip(dogFromDb, payload.microchipNumber, 1, transaction)
   await updateMicrochip(dogFromDb, payload.microchipNumber2, 2, transaction)
 }
@@ -34,6 +70,7 @@ const createMicrochip = async (microchipNumber, dogId, transaction) => {
   const microchip = {
     microchip_number: microchipNumber
   }
+
   const newMicrochip = await sequelize.models.microchip.create(microchip, { transaction })
   const dogMicrochip = {
     dog_id: dogId,
@@ -43,6 +80,8 @@ const createMicrochip = async (microchipNumber, dogId, transaction) => {
 }
 
 module.exports = {
+  updateMicrochip,
   updateMicrochips,
-  createMicrochip
+  createMicrochip,
+  microchipExists
 }
