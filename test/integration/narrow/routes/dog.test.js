342464fef3677a3277a4854d4cb34f9f1311baf7
Chris Cole <56303993+whitewaterdesign@users.noreply.github.com>
2024-05-20 16:34:20 +0100

    feat: 373491 - Prevent duplicate microchip entry when updateMicrochips (#143)
    * feat: 373491 - Prevent duplicate microchip entry when updateMicrochips
    
    * refactor: 373491 - Pull out duplicate check into throwForDuplicates
    
    * chore: patch v0.69.21
    
    * test: 373491 - Clean up dog endpoint tests
    
    * test: 373491 - Remove updateMicrochips test from dogs.test.js
    
    * test: 373491 - microchip.test.js clear mocks
    
    * chore: dummy commit
    
    * chore: patch v0.69.22
    
    * chore: patch v0.69.21
    
    * chore: dummy commit
    
    ---------
    
    Co-authored-by: Chris Cole <whitewaterdesign>

--- test/integration/narrow/routes/dog.test.js
+++ test/integration/narrow/routes/dog.test.js
@@ -17,206 +17,216 @@ describe('Dog endpoint', () => {
     await server.initialize()
   })
 
-  test('GET /dog/ED123 route returns 200', async () => {
-    getDogByIndexNumber.mockResolvedValue({ id: 123, indexNumber: 'ED123' })
+  describe('GET /dog/ED123', () => {
+    test('GET /dog/ED123 route returns 200', async () => {
+      getDogByIndexNumber.mockResolvedValue({ id: 123, indexNumber: 'ED123' })
 
-    const options = {
-      method: 'GET',
-      url: '/dog/ED123'
-    }
+      const options = {
+        method: 'GET',
+        url: '/dog/ED123'
+      }
 
-    const response = await server.inject(options)
-    expect(response.statusCode).toBe(200)
-  })
-
-  test('GET /dog/ED123 route returns 404 if dog does not exist', async () => {
-    getDogByIndexNumber.mockResolvedValue(null)
-
-    const options = {
-      method: 'GET',
-      url: '/dog/ED000'
-    }
-
-    const response = await server.inject(options)
-    expect(response.statusCode).toBe(404)
-  })
-
-  test('GET /dog/ED123 route returns 500 if db error', async () => {
-    getDogByIndexNumber.mockRejectedValue(new Error('Test error'))
-
-    const options = {
-      method: 'GET',
-      url: '/dog/ED123'
-    }
-
-    const response = await server.inject(options)
-
-    expect(response.statusCode).toBe(500)
-  })
-
-  test('GET /dog-owner/ED123 route returns 200', async () => {
-    getOwnerOfDog.mockResolvedValue({ person: { id: 123, personReference: 'P-123' } })
-
-    const options = {
-      method: 'GET',
-      url: '/dog-owner/ED123'
-    }
-
-    const response = await server.inject(options)
-    expect(response.statusCode).toBe(200)
-  })
-
-  test('GET /dog-owner/ED123 route returns 404 if dog owner does not exist', async () => {
-    getOwnerOfDog.mockResolvedValue(null)
-
-    const options = {
-      method: 'GET',
-      url: '/dog-owner/ED000'
-    }
-
-    const response = await server.inject(options)
-    expect(response.statusCode).toBe(404)
-  })
-
-  test('GET /dog-owner/ED123 route returns 404 if dog does not exist', async () => {
-    getDogByIndexNumber.mockResolvedValue(null)
-
-    const options = {
-      method: 'GET',
-      url: '/dog-owner/ED000'
-    }
-
-    const response = await server.inject(options)
-    expect(response.statusCode).toBe(404)
-  })
-
-  test('GET /dog-owner/ED123 route returns 500 if db error', async () => {
-    getOwnerOfDog.mockRejectedValue(new Error('Test error'))
-
-    const options = {
-      method: 'GET',
-      url: '/dog-owner/ED123'
-    }
-
-    const response = await server.inject(options)
-
-    expect(response.statusCode).toBe(500)
-  })
-
-  test('POST /dog route returns 400 with invalid payload', async () => {
-    addImportedDog.mockResolvedValue()
-
-    const options = {
-      method: 'POST',
-      url: '/dog',
-      payload: {}
-    }
-
-    const response = await server.inject(options)
-
-    expect(response.statusCode).toBe(400)
-  })
-
-  test('POST /dog route returns 200 with valid payload', async () => {
-    addImportedDog.mockResolvedValue()
-
-    const options = {
-      method: 'POST',
-      url: '/dog',
-      payload: { dog: { name: 'Bruno' } }
-    }
-
-    const response = await server.inject(options)
-
-    expect(response.statusCode).toBe(200)
-  })
-
-  test('PUT /dog route returns 400 with invalid payload', async () => {
-    updateDog.mockResolvedValue()
-
-    const options = {
-      method: 'PUT',
-      url: '/dog',
-      payload: {}
-    }
-
-    const response = await server.inject(options)
-
-    expect(response.statusCode).toBe(400)
-  })
-
-  test('PUT /dog route returns 500 with db error', async () => {
-    updateDog.mockImplementation(() => { throw new Error('dog error') })
-
-    const options = {
-      method: 'PUT',
-      url: '/dog',
-      payload: { indexNumber: 'ABC123' }
-    }
-
-    const response = await server.inject(options)
-
-    expect(response.statusCode).toBe(500)
-  })
-
-  test('PUT /dog route returns 200 with valid payload', async () => {
-    updateDog.mockResolvedValue()
-
-    const options = {
-      method: 'PUT',
-      url: '/dog',
-      payload: { indexNumber: 'ABC123' }
-    }
-
-    const response = await server.inject(options)
-
-    expect(response.statusCode).toBe(200)
-  })
-
-  test('DELETE /dog/ED123 route returns 204 with valid index', async () => {
-    getDogByIndexNumber.mockResolvedValue({ id: 123, indexNumber: 'ED123' })
-    getCallingUser.mockReturnValue({
-      username: 'internal-user',
-      displayname: 'User, Internal'
+      const response = await server.inject(options)
+      expect(response.statusCode).toBe(200)
     })
 
-    const options = {
-      method: 'DELETE',
-      url: '/dog/ED123'
-    }
+    test('GET /dog/ED123 route returns 404 if dog does not exist', async () => {
+      getDogByIndexNumber.mockResolvedValue(null)
 
-    const response = await server.inject(options)
+      const options = {
+        method: 'GET',
+        url: '/dog/ED000'
+      }
 
-    expect(response.statusCode).toBe(204)
-    expect(deleteDogByIndexNumber).toBeCalledWith('ED123', { displayname: 'User, Internal', username: 'internal-user' })
+      const response = await server.inject(options)
+      expect(response.statusCode).toBe(404)
+    })
+
+    test('GET /dog/ED123 route returns 500 if db error', async () => {
+      getDogByIndexNumber.mockRejectedValue(new Error('Test error'))
+
+      const options = {
+        method: 'GET',
+        url: '/dog/ED123'
+      }
+
+      const response = await server.inject(options)
+
+      expect(response.statusCode).toBe(500)
+    })
   })
 
-  test('DELETE /dog/ED123 route returns 404 with invalid dog index', async () => {
-    getDogByIndexNumber.mockResolvedValue(null)
+  describe('GET /dog-owner/ED123', () => {
+    test('GET /dog-owner/ED123 route returns 200', async () => {
+      getOwnerOfDog.mockResolvedValue({ person: { id: 123, personReference: 'P-123' } })
 
-    const options = {
-      method: 'DELETE',
-      url: '/dog/ED123'
-    }
+      const options = {
+        method: 'GET',
+        url: '/dog-owner/ED123'
+      }
 
-    const response = await server.inject(options)
+      const response = await server.inject(options)
+      expect(response.statusCode).toBe(200)
+    })
 
-    expect(response.statusCode).toBe(404)
-    expect(deleteDogByIndexNumber).not.toHaveBeenCalled()
+    test('GET /dog-owner/ED123 route returns 404 if dog owner does not exist', async () => {
+      getOwnerOfDog.mockResolvedValue(null)
+
+      const options = {
+        method: 'GET',
+        url: '/dog-owner/ED000'
+      }
+
+      const response = await server.inject(options)
+      expect(response.statusCode).toBe(404)
+    })
+
+    test('GET /dog-owner/ED123 route returns 404 if dog does not exist', async () => {
+      getDogByIndexNumber.mockResolvedValue(null)
+
+      const options = {
+        method: 'GET',
+        url: '/dog-owner/ED000'
+      }
+
+      const response = await server.inject(options)
+      expect(response.statusCode).toBe(404)
+    })
+
+    test('GET /dog-owner/ED123 route returns 500 if db error', async () => {
+      getOwnerOfDog.mockRejectedValue(new Error('Test error'))
+
+      const options = {
+        method: 'GET',
+        url: '/dog-owner/ED123'
+      }
+
+      const response = await server.inject(options)
+
+      expect(response.statusCode).toBe(500)
+    })
   })
 
-  test('DELETE /dog/ED123 route returns 500 given server error', async () => {
-    getDogByIndexNumber.mockRejectedValue(new Error('500 error'))
+  describe('POST /dog', () => {
+    test('POST /dog route returns 400 with invalid payload', async () => {
+      addImportedDog.mockResolvedValue()
 
-    const options = {
-      method: 'DELETE',
-      url: '/dog/ED123'
-    }
+      const options = {
+        method: 'POST',
+        url: '/dog',
+        payload: {}
+      }
 
-    const response = await server.inject(options)
+      const response = await server.inject(options)
 
-    expect(response.statusCode).toBe(500)
-    expect(deleteDogByIndexNumber).not.toHaveBeenCalled()
+      expect(response.statusCode).toBe(400)
+    })
+
+    test('POST /dog route returns 200 with valid payload', async () => {
+      addImportedDog.mockResolvedValue()
+
+      const options = {
+        method: 'POST',
+        url: '/dog',
+        payload: { dog: { name: 'Bruno' } }
+      }
+
+      const response = await server.inject(options)
+
+      expect(response.statusCode).toBe(200)
+    })
+  })
+
+  describe('PUT /dog', () => {
+    test('PUT /dog route returns 400 with invalid payload', async () => {
+      updateDog.mockResolvedValue()
+
+      const options = {
+        method: 'PUT',
+        url: '/dog',
+        payload: {}
+      }
+
+      const response = await server.inject(options)
+
+      expect(response.statusCode).toBe(400)
+    })
+
+    test('PUT /dog route returns 500 with db error', async () => {
+      updateDog.mockImplementation(() => { throw new Error('dog error') })
+
+      const options = {
+        method: 'PUT',
+        url: '/dog',
+        payload: { indexNumber: 'ABC123' }
+      }
+
+      const response = await server.inject(options)
+
+      expect(response.statusCode).toBe(500)
+    })
+
+    test('PUT /dog route returns 200 with valid payload', async () => {
+      updateDog.mockResolvedValue()
+
+      const options = {
+        method: 'PUT',
+        url: '/dog',
+        payload: { indexNumber: 'ABC123' }
+      }
+
+      const response = await server.inject(options)
+
+      expect(response.statusCode).toBe(200)
+    })
+  })
+
+  describe('', () => {
+    test('DELETE /dog/ED123 route returns 204 with valid index', async () => {
+      getDogByIndexNumber.mockResolvedValue({ id: 123, indexNumber: 'ED123' })
+      getCallingUser.mockReturnValue({
+        username: 'internal-user',
+        displayname: 'User, Internal'
+      })
+
+      const options = {
+        method: 'DELETE',
+        url: '/dog/ED123'
+      }
+
+      const response = await server.inject(options)
+
+      expect(response.statusCode).toBe(204)
+      expect(deleteDogByIndexNumber).toBeCalledWith('ED123', { displayname: 'User, Internal', username: 'internal-user' })
+    })
+
+    test('DELETE /dog/ED123 route returns 404 with invalid dog index', async () => {
+      getDogByIndexNumber.mockResolvedValue(null)
+
+      const options = {
+        method: 'DELETE',
+        url: '/dog/ED123'
+      }
+
+      const response = await server.inject(options)
+
+      expect(response.statusCode).toBe(404)
+      expect(deleteDogByIndexNumber).not.toHaveBeenCalled()
+    })
+
+    test('DELETE /dog/ED123 route returns 500 given server error', async () => {
+      getDogByIndexNumber.mockRejectedValue(new Error('500 error'))
+
+      const options = {
+        method: 'DELETE',
+        url: '/dog/ED123'
+      }
+
+      const response = await server.inject(options)
+
+      expect(response.statusCode).toBe(500)
+      expect(deleteDogByIndexNumber).not.toHaveBeenCalled()
+    })
   })
 
   afterEach(async () => {
---
+++ test/unit/app/repos/microchip.test.js
@@ -0,0 +1,166 @@
+const { DuplicateResourceError } = require('../../../../app/errors/duplicate-record')
+
+describe('microchip', () => {
+  jest.mock('../../../../app/config/db', () => ({
+    models: {
+      microchip: {
+        create: jest.fn(),
+        save: jest.fn(),
+        findOne: jest.fn(),
+        findAll: jest.fn()
+      },
+      dog_microchip: {
+        create: jest.fn()
+      }
+    },
+    where: jest.fn(),
+    transaction: jest.fn(),
+    col: jest.fn().mockReturnValue(''),
+    fn: jest.fn().mockReturnValue('')
+  }))
+  const sequelize = require('../../../../app/config/db')
+  const { Op } = require('sequelize')
+
+  const { microchipExists, updateMicrochips, updateMicrochip } = require('../../../../app/repos/microchip')
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe('updateMicrochip', () => {
+    test('should create new microchip given none exists', async () => {
+      sequelize.models.microchip.findAll.mockResolvedValue(null)
+      sequelize.models.microchip.create.mockResolvedValue({ id: '277890823930477' })
+
+      await updateMicrochip({ id: 1 }, '277890823930477', 1, {})
+      expect(sequelize.models.dog_microchip.create).toHaveBeenCalled()
+    })
+
+    test('should update microchip given one exists', async () => {
+      sequelize.models.microchip.findAll.mockResolvedValue([
+        {
+          id: 1,
+          microchip_number: '277890823930477',
+          dog_microchips: [{
+            id: 15,
+            dog_id: 300549,
+            microchip_id: 15
+          }]
+        }
+      ])
+    })
+  })
+
+  describe('updateMicrochips', () => {
+    test('should update existing', async () => {
+      const mockSave = jest.fn()
+      sequelize.models.microchip.findAll.mockResolvedValue([{ microchip_number: '123', save: mockSave }])
+
+      const dogFromDb = {
+        id: 1
+      }
+      const payload = {
+        microchipNumber: '456'
+      }
+      await updateMicrochips(dogFromDb, payload, {})
+      expect(mockSave).toHaveBeenCalledTimes(1)
+    })
+
+    test('should create new if not existing', async () => {
+      const mockSave = jest.fn()
+      sequelize.models.microchip.findAll.mockResolvedValue([])
+      sequelize.models.microchip.create.mockResolvedValue({ id: 101 })
+
+      const dogFromDb = {
+        id: 1
+      }
+      const payload = {
+        microchipNumber: '456'
+      }
+      await updateMicrochips(dogFromDb, payload, {})
+      expect(mockSave).toHaveBeenCalledTimes(0)
+      expect(sequelize.models.microchip.create).toHaveBeenCalledTimes(1)
+    })
+
+    test('should throw DuplicateResourceError is one microchip is a duplicates ', async () => {
+      sequelize.models.microchip.findOne.mockResolvedValueOnce({
+        id: 1,
+        microchip_number: '277890823930477',
+        dog_microchips: [{
+          id: 15,
+          dog_id: 300549,
+          microchip_id: 15
+        }]
+      })
+      sequelize.models.microchip.findOne.mockResolvedValueOnce(null)
+      const payload = {
+        microchipNumber: '277890823930477',
+        microchipNumber2: '759628280825931'
+      }
+      await expect(updateMicrochips(300550, payload, {})).rejects.toThrow(new DuplicateResourceError('The microchip number already exists', { microchipNumbers: ['277890823930477'] }))
+    })
+
+    test('should throw DuplicateResourceError is one microchip is a duplicates ', async () => {
+      sequelize.models.microchip.findOne.mockResolvedValueOnce({
+        id: 1,
+        microchip_number: '277890823930477',
+        dog_microchips: [{
+          id: 15,
+          dog_id: 300549,
+          microchip_id: 15
+        }]
+      })
+      sequelize.models.microchip.findOne.mockResolvedValueOnce({
+        id: 1,
+        microchip_number: '759628280825931',
+        dog_microchips: [{
+          id: 14,
+          dog_id: 300548,
+          microchip_id: 14
+        }]
+      })
+      const payload = {
+        microchipNumber: '277890823930477',
+        microchipNumber2: '759628280825931'
+      }
+      await expect(updateMicrochips(300550, payload, {})).rejects.toThrow(new DuplicateResourceError('The microchip number already exists', { microchipNumbers: ['277890823930477', '759628280825931'] }))
+    })
+  })
+
+  describe('microchipExists', () => {
+    test('should get microchip', async () => {
+      sequelize.models.microchip.findOne.mockResolvedValue({
+        id: 1,
+        microchip_number: '277890823930477',
+        dog_microchips: [{
+          id: 15,
+          dog_id: 300549,
+          microchip_id: 15
+        }]
+      })
+      const microchip = await microchipExists(5, '277890823930477', {})
+      expect(microchip).toEqual('277890823930477')
+      expect(sequelize.models.microchip.findOne).toHaveBeenCalledWith({
+        where: {
+          microchip_number: '277890823930477'
+        },
+        include: [{
+          model: sequelize.models.dog_microchip,
+          as: 'dog_microchips',
+          where: {
+            dog_id: {
+              [Op.not]: 5
+            }
+          }
+        }],
+        transaction: {}
+      })
+    })
+
+    test('should return null given microchip does not exist', async () => {
+      sequelize.models.microchip.findOne.mockResolvedValue(null)
+      const microchip = await microchipExists(2, '277890823930477', {})
+      expect(microchip).toEqual(null)
+    })
+  })
+})
