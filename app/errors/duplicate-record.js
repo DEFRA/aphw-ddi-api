2f8a28f80795e0c854e91e92da9a5a4089baf32c
Chris Cole <56303993+whitewaterdesign@users.noreply.github.com>
2024-05-20 16:34:20 +0100

    feat: 373491 - Prevent duplicate microchip entry when updateMicrochips (#143)
    * feat: 373491 - Prevent duplicate microchip entry when updateMicrochips
    
    * refactor: 373491 - Pull out duplicate check into throwForDuplicates
    
    * chore: patch v0.69.21
    
    * test: 373491 - Clean up dog endpoint tests
    
    * test: 373491 - Remove updateMicrochips test from dogs.test.js
    
    * test: 373491 - microchip.test.js clear mocks
    
    * chore: dummy commit
    
    * chore: patch v0.69.22
    
    * chore: patch v0.69.21
    
    * chore: dummy commit
    
    ---------
    
    Co-authored-by: Chris Cole <whitewaterdesign>

--- app/errors/duplicate-record.js
+++ app/errors/duplicate-record.js
@@ -1,10 +1,14 @@
 const { Boom } = require('@hapi/boom')
 
 class DuplicateResourceError extends Boom {
-  constructor (message) {
+  constructor (message, data = {}) {
     super(message, { statusCode: 409 })
     this.message = message
     this.name = 'DuplicateResourceError'
+    this.output.payload = {
+      ...this.output.payload,
+      ...data
+    }
     Object.setPrototypeOf(this, DuplicateResourceError.prototype)
   }
 }
--- app/repos/microchip.js
+++ app/repos/microchip.js
@@ -1,6 +1,46 @@
 const sequelize = require('../config/db')
+const { DuplicateResourceError } = require('../errors/duplicate-record')
+const { Op } = require('sequelize')
+
+const microchipExists = async (dogId, microchipNumber, transaction) => {
+  const microchip = await sequelize.models.microchip.findOne({
+    include: [{
+      model: sequelize.models.dog_microchip,
+      as: 'dog_microchips',
+      where: {
+        dog_id: {
+          [Op.not]: dogId
+        }
+      }
+    }],
+    where: {
+      microchip_number: microchipNumber
+    },
+    transaction
+  })
+
+  return microchip?.microchip_number ?? null
+}
+
+const throwForDuplicates = async (payload, dogId, transaction) => {
+  const duplicateMicrochips = []
+  const microchipsToCheck = [payload.microchipNumber, payload.microchipNumber2].filter(mC => !!mC)
+
+  for (const microchipNumber of microchipsToCheck) {
+    const duplicateMicrochip = await microchipExists(dogId, microchipNumber, transaction)
+    if (duplicateMicrochip !== null) {
+      duplicateMicrochips.push(duplicateMicrochip)
+    }
+  }
+
+  if (duplicateMicrochips.length) {
+    throw new DuplicateResourceError('The microchip number already exists', { microchipNumbers: duplicateMicrochips })
+  }
+}
 
 const updateMicrochips = async (dogFromDb, payload, transaction) => {
+  await throwForDuplicates(payload, dogFromDb.id, transaction)
+
   await updateMicrochip(dogFromDb, payload.microchipNumber, 1, transaction)
   await updateMicrochip(dogFromDb, payload.microchipNumber2, 2, transaction)
 }
@@ -34,6 +74,7 @@ const createMicrochip = async (microchipNumber, dogId, transaction) => {
   const microchip = {
     microchip_number: microchipNumber
   }
+
   const newMicrochip = await sequelize.models.microchip.create(microchip, { transaction })
   const dogMicrochip = {
     dog_id: dogId,
@@ -43,6 +84,8 @@ const createMicrochip = async (microchipNumber, dogId, transaction) => {
 }
 
 module.exports = {
+  updateMicrochip,
   updateMicrochips,
-  createMicrochip
+  createMicrochip,
+  microchipExists
 }
