ccfbcab688be6013126ba8e579d36cabdd50b191
work <jeremy.barnsley@esynergy.co.uk>
2024-05-24 09:30:07 +0100

    Storage tests

--- test/unit/app/storage/app-storage-def-cred.test.js
+++ test/unit/app/storage/app-storage-def-cred.test.js
@@ -7,8 +7,8 @@ describe('app storage', () => {
   beforeEach(() => {
     jest.resetModules()
 
-    jest.mock('@azure/storage-blob')
     jest.mock('@azure/identity')
+    jest.mock('@azure/storage-blob')
 
     blobServiceClient = require('@azure/storage-blob').BlobServiceClient
     defaultAzureCredential = require('@azure/identity').DefaultAzureCredential
@@ -20,18 +20,10 @@ describe('app storage', () => {
     process.env = DEFAULT_ENV
   })
 
-  test('should use connection string if useConnectionString true', () => {
-    process.env.AZURE_STORAGE_USE_CONNECTION_STRING = 'true'
-
-    require('../../../../app/storage/get-blob-client')
-
-    expect(blobServiceClient.fromConnectionString).toHaveBeenCalledTimes(1)
-  })
-
   test('should use DefaultAzureCredential if useConnectionString false', () => {
     process.env.AZURE_STORAGE_USE_CONNECTION_STRING = 'false'
 
-    require('../../../../app/storage/get-blob-client')
+    require('../../../../app/storage')
 
     expect(blobServiceClient).toHaveBeenCalledTimes(1)
     expect(defaultAzureCredential).toHaveBeenCalledTimes(1)
