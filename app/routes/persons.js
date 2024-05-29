1c3923da7512af7a760d4a5c61dd58a353b7ea3f
Chris Cole <56303993+whitewaterdesign@users.noreply.github.com>
2024-05-29 14:32:32 +0100

    Feat/327261  delete orphaned owners (#156)
    * feat: 327261 - GET /persons?orphaned=true endpoint
    
    * feat: 327261 - getPersons repo with limit and orphaned results
    
    * feat: 327261 - GET /persons?limit= query param
    
    * chore: patch v0.69.30
    
    * feat: 327261 - Disallow orphaned false on GET /persons schema
    
    * test: 327261 - Fix failing test GET /persons orphaned true
    
    * test: 327261 - Fix failing test GET /persons orphaned true 2
    
    * test: 327261 - Update GET /persons schema and getPersons interface to include sort keys
    
    * test: 327261 - getPersons sorting
    
    * chore: patch v0.69.31
    
    * feat: 327261 - Schema and endpoint for DELETE /persons
    
    * feat: 327261 - Delete response schema
    
    * feat: 327261 - Delete /persons endpoint
    
    * test: 327261 - Remove sortQueryParams from persons get test
    
    * feat: 327261 - Add DELETE /persons personReferences[] query params option
    
    * feat: 327261 - Swap DELETE /persons for POST /persons:batch-delete
    
    * chore: patch v0.69.34
    
    * test: 327261 - Fix failing test
    
    ---------
    
    Co-authored-by: Chris Cole <whitewaterdesign>

--- app/routes/persons.js
+++ app/routes/persons.js
@@ -1,6 +1,8 @@
 const { personsQueryParamsSchema } = require('../schema/persons/get')
-const { getPersons } = require('../repos/persons')
+const { getPersons, deletePersons } = require('../repos/persons')
 const { personDto } = require('../dto/person')
+const { deletePayloadSchema, deleteResponseSchema } = require('../schema/persons/delete')
+const { getCallingUser } = require('../auth/get-user')
 /**
  * @typedef GetPersonsQuery
  * @property {string} [firstName]
@@ -39,6 +41,32 @@ module.exports = [
         }
         return h.response(result).code(200)
       }
+    }
+  },
+  {
+    method: 'POST',
+    path: '/persons:batch-delete',
+    options: {
+      validate: {
+        payload: deletePayloadSchema,
+        failAction: (request, h, error) => {
+          return h.response().code(400).takeover()
+        }
+      },
+      response: {
+        schema: deleteResponseSchema,
+        failAction: (request, h, err) => {
+          console.error(err)
+
+          return h.response({ errors: err.details.map(e => e.message) }).code(400).takeover()
+        }
+      }
+    },
+    handler: async (request, h) => {
+      const personReferences = request.payload.personReferences
+
+      const result = await deletePersons(personReferences, getCallingUser(request))
+      return h.response(result).code(200)
     }
   }
 ]
