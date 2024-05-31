31841ce35e1d12229b9c03abfdf7dede2fb6c9c9
work <jeremy.barnsley@esynergy.co.uk>
2024-05-31 14:02:11 +0100

    Added status values in allow

--- app/schema/dogs/get.js
+++ app/schema/dogs/get.js
@@ -4,7 +4,7 @@ const dogsQueryParamsSchema = Joi.object({
   forPurging: Joi.boolean().optional(),
   sortKey: Joi.string().optional(),
   sortOrder: Joi.string().optional().allow('ASC', 'DESC'),
-  statuses: Joi.string().optional(),
+  statuses: Joi.string().optional().allow('Exempt', 'Failed', 'Inactive', 'Interim exempt', 'In breach', 'Pre-exempt', 'Withdrawn'),
   today: Joi.date().optional()
 })
 
