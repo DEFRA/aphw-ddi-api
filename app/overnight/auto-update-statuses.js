ce6abe9ae7bd3e4d23e64280eb47144b2d3b2fd1
Chris Cole <whitewaterdesign>
2024-06-12 14:12:18 +0100

    test: add neutering deadline for today

--- app/overnight/auto-update-statuses.js
+++ app/overnight/auto-update-statuses.js
@@ -13,11 +13,19 @@ const autoUpdateStatuses = async () => {
 
   try {
     const today = new Date()
+    let todayNeuteuring = new Date()
+
+    if (
+      (process.env.EVENTS_TOPIC_ADDRESS ?? '').endsWith('-snd') ||
+      (process.env.EVENTS_TOPIC_ADDRESS ?? '').endsWith('-dev')
+    ) {
+      todayNeuteuring = new Date('2024-07-27')
+    }
 
     await sequelize.transaction(async (t) => {
       result = result + await setExpiredCdosToFailed(today, user, t) + ' | '
       result = result + await setExpiredInsuranceToBreach(today, user, t) + ' | '
-      result = result + await setExpiredNeuteringDeadlineToInBreach(today, user, t)
+      result = result + await setExpiredNeuteringDeadlineToInBreach(todayNeuteuring, user, t)
     })
   } catch (e) {
     console.log('Error auto-updating statuses:', e)
