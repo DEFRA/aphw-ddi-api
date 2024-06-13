0b088f1c55e3c22664a6ac47021f659e756e9840
work <jeremy.barnsley@esynergy.co.uk>
2024-06-13 08:39:58 +0100

    Added documentation Changed status name to be 'name'

--- test/unit/app/dto/statistics.test.js
+++ test/unit/app/dto/statistics.test.js
@@ -7,13 +7,13 @@ describe('statistics Dto', () => {
     const dto = countsPerStatusDto(mockCountsPerStatus)
 
     const expectedDto = [
-      { status: { id: 4, status: 'Interim exempt' }, total: 20 },
-      { status: { id: 5, status: 'Pre-exempt' }, total: 30 },
-      { status: { id: 6, status: 'Failed' }, total: 40 },
-      { status: { id: 7, status: 'Exempt' }, total: 500 },
-      { status: { id: 8, status: 'In breach' }, total: 60 },
-      { status: { id: 9, status: 'Withdrawn' }, total: 70 },
-      { status: { id: 10, status: 'Inactive' }, total: 80 }
+      { status: { id: 4, name: 'Interim exempt' }, total: 20 },
+      { status: { id: 5, name: 'Pre-exempt' }, total: 30 },
+      { status: { id: 6, name: 'Failed' }, total: 40 },
+      { status: { id: 7, name: 'Exempt' }, total: 500 },
+      { status: { id: 8, name: 'In breach' }, total: 60 },
+      { status: { id: 9, name: 'Withdrawn' }, total: 70 },
+      { status: { id: 10, name: 'Inactive' }, total: 80 }
     ]
 
     expect(dto).toEqual(expectedDto)
@@ -27,13 +27,13 @@ describe('statistics Dto', () => {
     const dto = countsPerStatusDto(mockMissingCounts)
 
     const expectedDto = [
-      { status: { id: 4, status: 'Interim exempt' }, total: 0 },
-      { status: { id: 5, status: 'Pre-exempt' }, total: 30 },
-      { status: { id: 6, status: 'Failed' }, total: 0 },
-      { status: { id: 7, status: 'Exempt' }, total: 500 },
-      { status: { id: 8, status: 'In breach' }, total: 60 },
-      { status: { id: 9, status: 'Withdrawn' }, total: 70 },
-      { status: { id: 10, status: 'Inactive' }, total: 80 }
+      { status: { id: 4, name: 'Interim exempt' }, total: 0 },
+      { status: { id: 5, name: 'Pre-exempt' }, total: 30 },
+      { status: { id: 6, name: 'Failed' }, total: 0 },
+      { status: { id: 7, name: 'Exempt' }, total: 500 },
+      { status: { id: 8, name: 'In breach' }, total: 60 },
+      { status: { id: 9, name: 'Withdrawn' }, total: 70 },
+      { status: { id: 10, name: 'Inactive' }, total: 80 }
     ]
 
     expect(dto).toEqual(expectedDto)
