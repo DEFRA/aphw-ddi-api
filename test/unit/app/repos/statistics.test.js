0ec1e4c5fa1eaa474d99ce96bb36e55235f1d77f
Jez Barnsley <114290619+jbarnsley10@users.noreply.github.com>
2024-06-14 16:05:58 +0100

    Includes zero counts (#187)
    * Includes zero counts
    
    * Patch bump
    
    * Upgraded json-difference
    Removed snyk exclusion
    
    ---------
    
    Co-authored-by: work <jeremy.barnsley@esynergy.co.uk>

--- test/unit/app/repos/statistics.test.js
+++ test/unit/app/repos/statistics.test.js
@@ -3,11 +3,12 @@ const { countsPerStatus: mockCountsPerStatus } = require('../../../mocks/statist
 describe('Statistics repo', () => {
   jest.mock('../../../../app/config/db', () => ({
     models: {
-      dog: {
+      status: {
         findAll: jest.fn()
       }
     },
     fn: jest.fn(),
+    col: jest.fn(),
     literal: jest.fn()
   }))
 
@@ -21,25 +22,25 @@ describe('Statistics repo', () => {
 
   describe('getCountsPerStatus', () => {
     test('should return counts per status', async () => {
-      sequelize.models.dog.findAll.mockResolvedValue(mockCountsPerStatus)
+      sequelize.models.status.findAll.mockResolvedValue(mockCountsPerStatus)
 
       const counts = await getCountsPerStatus()
 
       expect(counts).toHaveLength(7)
       expect(counts[0].total).toBe('20')
-      expect(counts[0].status.status).toBe('Interim exempt')
+      expect(counts[0].status).toBe('Interim exempt')
       expect(counts[1].total).toBe('30')
-      expect(counts[1].status.status).toBe('Pre-exempt')
+      expect(counts[1].status).toBe('Pre-exempt')
       expect(counts[2].total).toBe('40')
-      expect(counts[2].status.status).toBe('Failed')
+      expect(counts[2].status).toBe('Failed')
       expect(counts[3].total).toBe('500')
-      expect(counts[3].status.status).toBe('Exempt')
+      expect(counts[3].status).toBe('Exempt')
       expect(counts[4].total).toBe('60')
-      expect(counts[4].status.status).toBe('In breach')
+      expect(counts[4].status).toBe('In breach')
       expect(counts[5].total).toBe('70')
-      expect(counts[5].status.status).toBe('Withdrawn')
+      expect(counts[5].status).toBe('Withdrawn')
       expect(counts[6].total).toBe('80')
-      expect(counts[6].status.status).toBe('Inactive')
+      expect(counts[6].status).toBe('Inactive')
     })
   })
 
