59bfdc83d0dbf37c0cec346d45f7541bbea016c6
Chris Cole <whitewaterdesign>
2024-06-14 09:14:45 +0100

    revert: 360433 - Reset autoUpdateStatuses fixed date

--- app/overnight/auto-update-statuses.js
+++ app/overnight/auto-update-statuses.js
@@ -2,7 +2,6 @@ const sequelize = require('../config/db')
 const { setExpiredCdosToFailed } = require('./expired-cdo')
 const { setExpiredInsuranceToBreach } = require('./expired-insurance')
 const { setExpiredNeuteringDeadlineToInBreach } = require('./expired-neutering-deadline')
-const { getEnvironmentVariableOrString } = require('../lib/environment-helpers')
 
 const autoUpdateStatuses = async () => {
   let result = ''
@@ -14,19 +13,11 @@ const autoUpdateStatuses = async () => {
 
   try {
     const today = new Date()
-    let todayNeuteuring = new Date()
-
-    if (
-      getEnvironmentVariableOrString('EVENTS_TOPIC_ADDRESS').endsWith('-snd') ||
-      getEnvironmentVariableOrString('EVENTS_TOPIC_ADDRESS').endsWith('-dev')
-    ) {
-      todayNeuteuring = new Date('2024-07-27')
-    }
 
     await sequelize.transaction(async (t) => {
       result = result + await setExpiredCdosToFailed(today, user, t) + ' | '
       result = result + await setExpiredInsuranceToBreach(today, user, t) + ' | '
-      result = result + await setExpiredNeuteringDeadlineToInBreach(todayNeuteuring, user, t)
+      result = result + await setExpiredNeuteringDeadlineToInBreach(today, user, t)
     })
   } catch (e) {
     console.log('Error auto-updating statuses:', e)
