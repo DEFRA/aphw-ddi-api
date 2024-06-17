05e933fa546a760f7020564693a0cc31762fe1fd
Chris Cole <whitewaterdesign>
2024-06-17 17:58:22 +0100

    feat: 383555 - Setup purge paranoidRetentionPeriod

--- app/overnight/purge-soft-deleted-records.js
+++ app/overnight/purge-soft-deleted-records.js
@@ -1,3 +1,5 @@
+const { paranoidRetentionPeriod } = require('../config/index')
+
 /**
  * @typedef {{
  *         count: {
@@ -12,20 +14,68 @@
  *       }} SoftDeletedRecordPayload
  */
 
+const sequelize = require('../config/db')
+const { Op } = require('sequelize')
 /**
  * @param {Date} date
  * @return {Promise<SoftDeletedRecordPayload>}
  */
 const purgeSoftDeletedRecords = async (date) => {
-  return {
-    count: {
-      dogs: 0,
-      owners: 0,
-      total: 0
+  const deleteAtCutOff = new Date(date)
+  deleteAtCutOff.setUTCDate(deleteAtCutOff.getUTCDate() - paranoidRetentionPeriod)
+
+  const ownerResults = await sequelize.models.person.findAll({
+    where: {
+      deleted_at: {
+        [Op.lte]: deleteAtCutOff
+      }
     },
+    paranoid: false
+  })
+
+  const count = {
+    dogs: 0,
+    owners: 0,
+    get total () {
+      return this.dogs + this.owners
+    }
+  }
+  const owners = []
+
+  for (const owner of ownerResults) {
+    try {
+      owners.push(owner.person_reference)
+      count.owners++
+    } catch (e) {
+      console.log(`Failed to hard delete owner record ${owner.person_reference}`, e)
+    }
+  }
+
+  const dogs = []
+
+  const dogResults = await sequelize.models.dog.findAll({
+    where: {
+      deleted_at: {
+        [Op.lte]: deleteAtCutOff
+      }
+    },
+    paranoid: false
+  })
+
+  for (const dog of dogResults) {
+    try {
+      dogs.push(dog.index_number)
+      count.dogs++
+    } catch (e) {
+      console.log(`Failed to hard delete dog record ${dog.index_number}`, e)
+    }
+  }
+
+  return {
+    count,
     deleted: {
-      dogs: [],
-      owners: []
+      dogs,
+      owners
     }
   }
 }
--- test/unit/app/overnight/purge-soft-deleted-records.test.js
+++ test/unit/app/overnight/purge-soft-deleted-records.test.js
@@ -1,17 +1,88 @@
-const { purgeSoftDeletedRecords } = require('../../../../app/overnight/purge-soft-deleted-records')
 describe('purge-soft-deleted-records', () => {
+  jest.mock('../../../../app/config/db', () => ({
+    models: {
+      dog: {
+        findByPk: jest.fn(),
+        create: jest.fn(),
+        findOne: jest.fn(),
+        findAll: jest.fn(),
+        destroy: jest.fn()
+      },
+      person: {
+        findAll: jest.fn(),
+        destroy: jest.fn()
+      },
+      registration: {
+        findOne: jest.fn(),
+        findByPk: jest.fn(),
+        findAll: jest.fn(),
+        create: jest.fn(),
+        destroy: jest.fn()
+      },
+      registered_person: {
+        create: jest.fn(),
+        findOne: jest.fn()
+      },
+      microchip: {
+        findOne: jest.fn(),
+        findAll: jest.fn(),
+        create: jest.fn()
+      },
+      dog_microchip: {
+        findAll: jest.fn(),
+        create: jest.fn()
+      },
+      status: {
+        findAll: jest.fn()
+      },
+      search_index: {
+        findAll: jest.fn(),
+        save: jest.fn()
+      }
+    },
+    col: jest.fn(),
+    transaction: jest.fn(),
+    literal: jest.fn()
+  }))
+  const sequelize = require('../../../../app/config/db')
+  const { Op } = require('sequelize')
+
+  const { purgeSoftDeletedRecords } = require('../../../../app/overnight/purge-soft-deleted-records')
+
   describe('purgeSoftDeletedRecords', () => {
-    test('should be a function', async () => {
-      const result = await purgeSoftDeletedRecords(new Date())
+    test('should purge soft deleted records', async () => {
+      sequelize.models.person.findAll.mockResolvedValue([{
+        person_reference: 'P-1234-56'
+      }])
+      sequelize.models.dog.findAll.mockResolvedValue([{
+        index_number: 'ED300002'
+      }])
+      const result = await purgeSoftDeletedRecords(new Date('2024-06-17T00:00:00.000Z'))
+      expect(sequelize.models.person.findAll).toHaveBeenCalledWith({
+        where: {
+          deleted_at: {
+            [Op.lte]: new Date('2024-03-19T00:00:00.000Z')
+          }
+        },
+        paranoid: false
+      })
+      expect(sequelize.models.dog.findAll).toHaveBeenCalledWith({
+        where: {
+          deleted_at: {
+            [Op.lte]: new Date('2024-03-19T00:00:00.000Z')
+          }
+        },
+        paranoid: false
+      })
       expect(result).toEqual({
         count: {
-          dogs: 0,
-          owners: 0,
-          total: 0
+          dogs: 1,
+          owners: 1,
+          total: 2
         },
         deleted: {
-          dogs: [],
-          owners: []
+          dogs: ['ED300002'],
+          owners: ['P-1234-56']
         }
       })
     })
