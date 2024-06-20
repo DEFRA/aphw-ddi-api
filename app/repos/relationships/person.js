d756658b6068aaf4bc539851cc0805fca79c28be
Chris Cole <56303993+whitewaterdesign@users.noreply.github.com>
2024-06-20 15:01:34 +0100

    Bugfix/383555  permanently delete bugs (#191)
    * fix: 383555 - Soft/Hard delete insurance on dog delete
    
    * fix: 383555 - Soft delete addresses multiple
    
    * chore: patch v0.71.19
    
    ---------
    
    Co-authored-by: Chris Cole <whitewaterdesign>

--- app/repos/relationships/person.js
+++ app/repos/relationships/person.js
@@ -1,49 +1,58 @@
 const { Op } = require('sequelize')
 
-const personRelationship = (sequelize) => [{
-  model: sequelize.models.person_address,
-  as: 'addresses',
-  where: {
-    id: {
-      [Op.in]: sequelize.literal('(SELECT max(id) as id FROM person_address GROUP BY person_id)')
-    }
-  },
-  include: [
+const personRelationship = (sequelize, latestAddress = true) => {
+  const addressWhere = latestAddress
+    ? {
+        where: {
+          id: {
+            [Op.in]: sequelize.literal('(SELECT max(id) as id FROM person_address GROUP BY person_id)')
+          }
+        }
+      }
+    : {}
+
+  return [
     {
-      model: sequelize.models.address,
-      as: 'address',
+      model: sequelize.models.person_address,
+      as: 'addresses',
+      ...addressWhere,
       include: [
         {
-          attribute: ['country'],
-          model: sequelize.models.country,
-          as: 'country'
+          model: sequelize.models.address,
+          as: 'address',
+          include: [
+            {
+              attribute: ['country'],
+              model: sequelize.models.country,
+              as: 'country'
+            }
+          ]
         }
       ]
-    }
-  ]
-},
-{
-  model: sequelize.models.person_contact,
-  as: 'person_contacts',
-  separate: true, // workaround to prevent 'contact_type_id' being truncated to 'contact_type_i'
-  include: [
+    },
     {
-      model: sequelize.models.contact,
-      as: 'contact',
+      model: sequelize.models.person_contact,
+      as: 'person_contacts',
+      separate: true, // workaround to prevent 'contact_type_id' being truncated to 'contact_type_i'
       include: [
         {
-          model: sequelize.models.contact_type,
-          as: 'contact_type'
+          model: sequelize.models.contact,
+          as: 'contact',
+          include: [
+            {
+              model: sequelize.models.contact_type,
+              as: 'contact_type'
+            }
+          ]
         }
       ]
+    },
+    {
+      model: sequelize.models.organisation,
+      as: 'organisation'
     }
   ]
-},
-{
-  model: sequelize.models.organisation,
-  as: 'organisation'
 }
-]
 
 module.exports = {
   personRelationship
