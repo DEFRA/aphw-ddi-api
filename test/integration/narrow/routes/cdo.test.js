9b70dd92b4f4fe4e8ed0641d2add9440a215e3f9
Chris Cole <56303993+whitewaterdesign@users.noreply.github.com>
2024-06-24 10:39:56 +0100

    feat: 279887 - Manage Cdo skeleton endpoint with fixed return (#193)
    * feat: 279887 - Manage Cdo skeleton endpoint with fixed return
    
    * chore: patch v0.71.21
    
    * test: 279887 - Improve coverage for manage cdo route tests
    
    ---------
    
    Co-authored-by: Chris Cole <whitewaterdesign>

--- test/integration/narrow/routes/cdo.test.js
+++ test/integration/narrow/routes/cdo.test.js
@@ -14,246 +14,376 @@ describe('CDO endpoint', () => {
     await server.initialize()
   })
 
-  test('GET /cdo/ED123 route returns 200', async () => {
-    getCdo.mockResolvedValue({
-      id: 123,
-      indexNumber: 'ED123',
-      dog_breed: {
-        breed: 'breed1'
-      },
-      status: {
-        status: 'NEW'
-      },
-      registration: {
-        court: {
-          name: 'court1'
+  describe('GET /cdo/ED123', () => {
+    test('GET /cdo/ED123 route returns 200', async () => {
+      getCdo.mockResolvedValue({
+        id: 123,
+        indexNumber: 'ED123',
+        dog_breed: {
+          breed: 'breed1'
         },
-        police_force: {
-          name: 'force1'
+        status: {
+          status: 'NEW'
         },
-        exemption_order: {
-          exemption_order: 2015
-        }
-      },
-      registered_person: [{
-        person: {
-        }
-      }]
-    })
-
-    const options = {
-      method: 'GET',
-      url: '/cdo/ED123'
-    }
-
-    const response = await server.inject(options)
-    expect(response.statusCode).toBe(200)
-  })
-
-  test('GET /cdo/ED123 route returns 500 when error', async () => {
-    getCdo.mockImplementation(() => { throw new Error('cdo error') })
-
-    const options = {
-      method: 'GET',
-      url: '/cdo/ED123'
-    }
-
-    const response = await server.inject(options)
-    expect(response.statusCode).toBe(500)
-  })
-
-  test('POST /cdo route returns 200 with valid payload', async () => {
-    const options = {
-      method: 'POST',
-      url: '/cdo',
-      payload: mockCreatePayload
-    }
-
-    createCdo.mockResolvedValue({
-      owner: {
-        first_name: 'John',
-        last_name: 'Doe',
-        birth_date: '1990-01-01',
-        address: {
-          address_line_1: '1 Test Street',
-          address_line_2: 'Test',
-          town: 'Test',
-          postcode: 'TE1 1ST',
-          country: {
-            country: 'England'
-          }
-        },
-        person_reference: '1234'
-      },
-      dogs: [
-        {
-          index_number: 'ED10000',
-          name: 'Test Dog',
-          dog_breed: {
-            breed: 'Test Breed'
+        registration: {
+          court: {
+            name: 'court1'
           },
-          registration: {
-            police_force: {
-              name: 'Test Police Force'
-            },
-            court: {
-              name: 'Test Court'
-            },
-            cdo_issued: '2020-01-01',
-            cdo_expiry: '2020-02-01',
-            legislation_officer: 'Test Officer'
-          }
-        }
-      ]
-    })
-
-    const response = await server.inject(options)
-    expect(response.statusCode).toBe(200)
-    expect(response.result).toEqual({
-      owner: {
-        firstName: 'John',
-        lastName: 'Doe',
-        birthDate: '1990-01-01',
-        address: {
-          addressLine1: '1 Test Street',
-          addressLine2: 'Test',
-          town: 'Test',
-          postcode: 'TE1 1ST',
-          country: 'England'
-        },
-        personReference: '1234'
-      },
-      enforcementDetails: {
-        policeForce: 'Test Police Force',
-        court: 'Test Court',
-        legislationOfficer: 'Test Officer'
-      },
-      dogs: [
-        {
-          indexNumber: 'ED10000',
-          name: 'Test Dog',
-          breed: 'Test Breed',
-          cdoIssued: '2020-01-01',
-          cdoExpiry: '2020-02-01'
-        }
-      ]
-    })
-  })
-
-  test('POST /cdo route returns 200 with valid payload and personReference', async () => {
-    const options = {
-      method: 'POST',
-      url: '/cdo',
-      payload: mockCreateWithRefPayload
-    }
-
-    createCdo.mockResolvedValue({
-      owner: {
-        first_name: 'John',
-        last_name: 'Doe',
-        birth_date: '1990-01-01',
-        address: {
-          address_line_1: '1 Test Street',
-          address_line_2: 'Test',
-          town: 'Test',
-          postcode: 'TE1 1ST',
-          country: {
-            country: 'England'
-          }
-        },
-        person_reference: 'P-6076-A37C'
-      },
-      dogs: [
-        {
-          index_number: 'ED10000',
-          name: 'Test Dog',
-          dog_breed: {
-            breed: 'Test Breed'
+          police_force: {
+            name: 'force1'
           },
-          registration: {
-            police_force: {
-              name: 'Test Police Force'
+          exemption_order: {
+            exemption_order: 2015
+          }
+        },
+        registered_person: [{
+          person: {
+          }
+        }]
+      })
+
+      const options = {
+        method: 'GET',
+        url: '/cdo/ED123'
+      }
+
+      const response = await server.inject(options)
+      expect(response.statusCode).toBe(200)
+    })
+
+    test('GET /cdo/ED123 route returns 404 when not found', async () => {
+      getCdo.mockResolvedValue(null)
+
+      const options = {
+        method: 'GET',
+        url: '/cdo/ED123'
+      }
+
+      const response = await server.inject(options)
+      expect(response.statusCode).toBe(404)
+    })
+
+    test('GET /cdo/ED123 route returns 500 when error', async () => {
+      getCdo.mockImplementation(() => { throw new Error('cdo error') })
+
+      const options = {
+        method: 'GET',
+        url: '/cdo/ED123'
+      }
+
+      const response = await server.inject(options)
+      expect(response.statusCode).toBe(500)
+    })
+  })
+
+  describe('POST /cdo', () => {
+    test('POST /cdo route returns 200 with valid payload', async () => {
+      const options = {
+        method: 'POST',
+        url: '/cdo',
+        payload: mockCreatePayload
+      }
+
+      createCdo.mockResolvedValue({
+        owner: {
+          first_name: 'John',
+          last_name: 'Doe',
+          birth_date: '1990-01-01',
+          address: {
+            address_line_1: '1 Test Street',
+            address_line_2: 'Test',
+            town: 'Test',
+            postcode: 'TE1 1ST',
+            country: {
+              country: 'England'
+            }
+          },
+          person_reference: '1234'
+        },
+        dogs: [
+          {
+            index_number: 'ED10000',
+            name: 'Test Dog',
+            dog_breed: {
+              breed: 'Test Breed'
             },
-            court: {
-              name: 'Test Court'
+            registration: {
+              police_force: {
+                name: 'Test Police Force'
+              },
+              court: {
+                name: 'Test Court'
+              },
+              cdo_issued: '2020-01-01',
+              cdo_expiry: '2020-02-01',
+              legislation_officer: 'Test Officer'
+            }
+          }
+        ]
+      })
+
+      const response = await server.inject(options)
+      expect(response.statusCode).toBe(200)
+      expect(response.result).toEqual({
+        owner: {
+          firstName: 'John',
+          lastName: 'Doe',
+          birthDate: '1990-01-01',
+          address: {
+            addressLine1: '1 Test Street',
+            addressLine2: 'Test',
+            town: 'Test',
+            postcode: 'TE1 1ST',
+            country: 'England'
+          },
+          personReference: '1234'
+        },
+        enforcementDetails: {
+          policeForce: 'Test Police Force',
+          court: 'Test Court',
+          legislationOfficer: 'Test Officer'
+        },
+        dogs: [
+          {
+            indexNumber: 'ED10000',
+            name: 'Test Dog',
+            breed: 'Test Breed',
+            cdoIssued: '2020-01-01',
+            cdoExpiry: '2020-02-01'
+          }
+        ]
+      })
+    })
+
+    test('POST /cdo route returns 200 with valid payload and personReference', async () => {
+      const options = {
+        method: 'POST',
+        url: '/cdo',
+        payload: mockCreateWithRefPayload
+      }
+
+      createCdo.mockResolvedValue({
+        owner: {
+          first_name: 'John',
+          last_name: 'Doe',
+          birth_date: '1990-01-01',
+          address: {
+            address_line_1: '1 Test Street',
+            address_line_2: 'Test',
+            town: 'Test',
+            postcode: 'TE1 1ST',
+            country: {
+              country: 'England'
+            }
+          },
+          person_reference: 'P-6076-A37C'
+        },
+        dogs: [
+          {
+            index_number: 'ED10000',
+            name: 'Test Dog',
+            dog_breed: {
+              breed: 'Test Breed'
             },
-            cdo_issued: '2020-01-01',
-            cdo_expiry: '2020-02-01',
-            legislation_officer: 'Test Officer'
+            registration: {
+              police_force: {
+                name: 'Test Police Force'
+              },
+              court: {
+                name: 'Test Court'
+              },
+              cdo_issued: '2020-01-01',
+              cdo_expiry: '2020-02-01',
+              legislation_officer: 'Test Officer'
+            }
+          }
+        ]
+      })
+
+      const response = await server.inject(options)
+      expect(response.statusCode).toBe(200)
+      expect(response.result).toEqual({
+        owner: {
+          firstName: 'John',
+          lastName: 'Doe',
+          birthDate: '1990-01-01',
+          address: {
+            addressLine1: '1 Test Street',
+            addressLine2: 'Test',
+            town: 'Test',
+            postcode: 'TE1 1ST',
+            country: 'England'
+          },
+          personReference: 'P-6076-A37C'
+        },
+        enforcementDetails: {
+          policeForce: 'Test Police Force',
+          court: 'Test Court',
+          legislationOfficer: 'Test Officer'
+        },
+        dogs: [
+          {
+            indexNumber: 'ED10000',
+            name: 'Test Dog',
+            breed: 'Test Breed',
+            cdoIssued: '2020-01-01',
+            cdoExpiry: '2020-02-01'
+          }
+        ]
+      })
+    })
+
+    test('POST /cdo route returns 422 with invalid personReference', async () => {
+      const options = {
+        method: 'POST',
+        url: '/cdo',
+        payload: mockCreateWithRefPayload
+      }
+
+      createCdo.mockRejectedValue(new NotFoundError('Owner not found'))
+
+      const response = await server.inject(options)
+
+      expect(response.statusCode).toBe(422)
+    })
+
+    test('POST /cdo route returns 400 with invalid payload', async () => {
+      const options = {
+        method: 'POST',
+        url: '/cdo',
+        payload: {}
+      }
+
+      const response = await server.inject(options)
+      expect(response.statusCode).toBe(400)
+    })
+
+    test('POST /cdo route returns 500 if db error', async () => {
+      createCdo.mockRejectedValue(new Error('Test error'))
+
+      const options = {
+        method: 'POST',
+        url: '/cdo',
+        payload: mockCreatePayload
+      }
+
+      const response = await server.inject(options)
+
+      expect(response.statusCode).toBe(500)
+    })
+  })
+
+  describe('GET /cdo/ED123/manage', () => {
+    test('should return an initialised manage cdo task list', async () => {
+      getCdo.mockResolvedValue({
+        id: 123,
+        indexNumber: 'ED123',
+        dog_breed: {
+          breed: 'breed1'
+        },
+        status: {
+          status: 'NEW'
+        },
+        registration: {
+          court: {
+            name: 'court1'
+          },
+          police_force: {
+            name: 'force1'
+          },
+          exemption_order: {
+            exemption_order: 2015
+          }
+        },
+        registered_person: [{
+          person: {
+          }
+        }]
+      })
+      const options = {
+        method: 'GET',
+        url: '/cdo/ED123/manage'
+      }
+
+      const response = await server.inject(options)
+      const payload = JSON.parse(response.payload)
+      expect(response.statusCode).toBe(200)
+      expect(payload).toEqual({
+        tasks: {
+          applicationPackSent: {
+            key: 'applicationPackSent',
+            available: true,
+            completed: false,
+            editable: true,
+            timestamp: undefined
+          },
+          insuranceDetailsRecorded: {
+            key: 'insuranceDetailsRecorded',
+            available: false,
+            completed: false,
+            editable: false,
+            timestamp: undefined
+          },
+          microchipNumberRecorded: {
+            key: 'microchipNumberRecorded',
+            available: false,
+            completed: false,
+            editable: false,
+            timestamp: undefined
+          },
+          applicationFeePaid: {
+            key: 'applicationFeePaid',
+            available: false,
+            completed: false,
+            editable: false,
+            timestamp: undefined
+          },
+          form2Sent: {
+            key: 'form2Sent',
+            available: false,
+            completed: false,
+            editable: false,
+            timestamp: undefined
+          },
+          verificationDateRecorded: {
+            key: 'verificationDateRecorded',
+            available: false,
+            completed: false,
+            editable: false,
+            timestamp: undefined
+          },
+          certificateIssued: {
+            key: 'certificateIssued',
+            available: false,
+            completed: false,
+            editable: false,
+            timestamp: undefined
           }
         }
-      ]
+      })
     })
 
-    const response = await server.inject(options)
-    expect(response.statusCode).toBe(200)
-    expect(response.result).toEqual({
-      owner: {
-        firstName: 'John',
-        lastName: 'Doe',
-        birthDate: '1990-01-01',
-        address: {
-          addressLine1: '1 Test Street',
-          addressLine2: 'Test',
-          town: 'Test',
-          postcode: 'TE1 1ST',
-          country: 'England'
-        },
-        personReference: 'P-6076-A37C'
-      },
-      enforcementDetails: {
-        policeForce: 'Test Police Force',
-        court: 'Test Court',
-        legislationOfficer: 'Test Officer'
-      },
-      dogs: [
-        {
-          indexNumber: 'ED10000',
-          name: 'Test Dog',
-          breed: 'Test Breed',
-          cdoIssued: '2020-01-01',
-          cdoExpiry: '2020-02-01'
-        }
-      ]
+    test('should throw a 404 given index does not exist', async () => {
+      getCdo.mockResolvedValue(null)
+      const options = {
+        method: 'GET',
+        url: '/cdo/ED123/manage'
+      }
+
+      const response = await server.inject(options)
+      expect(response.statusCode).toBe(404)
     })
-  })
 
-  test('POST /cdo route returns 422 with invalid personReference', async () => {
-    const options = {
-      method: 'POST',
-      url: '/cdo',
-      payload: mockCreateWithRefPayload
-    }
+    test('should returns 500 given server error thrown', async () => {
+      getCdo.mockImplementation(() => { throw new Error('cdo error') })
 
-    createCdo.mockRejectedValue(new NotFoundError('Owner not found'))
+      const options = {
+        method: 'GET',
+        url: '/cdo/ED123/manage'
+      }
 
-    const response = await server.inject(options)
-
-    expect(response.statusCode).toBe(422)
-  })
-
-  test('POST /cdo route returns 400 with invalid payload', async () => {
-    const options = {
-      method: 'POST',
-      url: '/cdo',
-      payload: {}
-    }
-
-    const response = await server.inject(options)
-    expect(response.statusCode).toBe(400)
-  })
-
-  test('POST /cdo route returns 500 if db error', async () => {
-    createCdo.mockRejectedValue(new Error('Test error'))
-
-    const options = {
-      method: 'POST',
-      url: '/cdo',
-      payload: mockCreatePayload
-    }
-
-    const response = await server.inject(options)
-
-    expect(response.statusCode).toBe(500)
+      const response = await server.inject(options)
+      expect(response.statusCode).toBe(500)
+    })
   })
 })
