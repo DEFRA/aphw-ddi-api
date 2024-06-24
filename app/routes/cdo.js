496ed216e2eaf9ed2e4e916534fad5edfdb40474
Chris Cole <56303993+whitewaterdesign@users.noreply.github.com>
2024-06-24 10:39:56 +0100

    feat: 279887 - Manage Cdo skeleton endpoint with fixed return (#193)
    * feat: 279887 - Manage Cdo skeleton endpoint with fixed return
    
    * chore: patch v0.71.21
    
    * test: 279887 - Improve coverage for manage cdo route tests
    
    ---------
    
    Co-authored-by: Chris Cole <whitewaterdesign>

--- app/routes/cdo.js
+++ app/routes/cdo.js
@@ -4,48 +4,121 @@ const { getCallingUser } = require('../auth/get-user')
 const cdoCreateSchema = require('../schema/cdo/create')
 const { NotFoundError } = require('../errors/not-found')
 
-module.exports = [{
-  method: 'GET',
-  path: '/cdo/{indexNumber}',
-  handler: async (request, h) => {
-    const indexNumber = request.params.indexNumber
-    try {
-      const cdo = await getCdo(indexNumber)
-
-      if (!cdo) {
-        return h.response().code(404)
-      }
-
-      return h.response({ cdo: cdoViewDto(cdo) }).code(200)
-    } catch (e) {
-      console.log('Error retrieving cdo record:', e)
-      throw e
-    }
-  }
-},
-{
-  method: 'POST',
-  path: '/cdo',
-  options: {
-    validate: {
-      payload: cdoCreateSchema,
-      failAction: (request, h, err) => {
-        console.error(err)
-
-        return h.response({ errors: err.details.map(e => e.message) }).code(400).takeover()
-      }
-    },
+module.exports = [
+  {
+    method: 'GET',
+    path: '/cdo/{indexNumber}',
     handler: async (request, h) => {
+      const indexNumber = request.params.indexNumber
       try {
-        const created = await createCdo(request.payload, getCallingUser(request))
-        const res = cdoCreateDto(created)
-        return h.response(res).code(200)
-      } catch (e) {
-        if (e instanceof NotFoundError) {
-          return h.response().code(422).takeover()
+        const cdo = await getCdo(indexNumber)
+
+        if (!cdo) {
+          return h.response().code(404)
         }
+
+        return h.response({ cdo: cdoViewDto(cdo) }).code(200)
+      } catch (e) {
+        console.log('Error retrieving cdo record:', e)
+        throw e
+      }
+    }
+  },
+  {
+    method: 'POST',
+    path: '/cdo',
+    options: {
+      validate: {
+        payload: cdoCreateSchema,
+        failAction: (request, h, err) => {
+          console.error(err)
+
+          return h.response({ errors: err.details.map(e => e.message) }).code(400).takeover()
+        }
+      },
+      handler: async (request, h) => {
+        try {
+          const created = await createCdo(request.payload, getCallingUser(request))
+          const res = cdoCreateDto(created)
+          return h.response(res).code(200)
+        } catch (e) {
+          if (e instanceof NotFoundError) {
+            return h.response().code(422).takeover()
+          }
+          throw e
+        }
+      }
+    }
+  },
+  {
+    method: 'GET',
+    path: '/cdo/{indexNumber}/manage',
+    handler: async (request, h) => {
+      const indexNumber = request.params.indexNumber
+      try {
+        const cdo = await getCdo(indexNumber)
+
+        if (!cdo) {
+          return h.response().code(404)
+        }
+
+        const tasks = {
+          applicationPackSent: {
+            key: 'applicationPackSent',
+            available: true,
+            completed: false,
+            editable: true,
+            timestamp: undefined
+          },
+          insuranceDetailsRecorded: {
+            key: 'insuranceDetailsRecorded',
+            available: false,
+            completed: false,
+            editable: false,
+            timestamp: undefined
+          },
+          microchipNumberRecorded: {
+            key: 'microchipNumberRecorded',
+            available: false,
+            completed: false,
+            editable: false,
+            timestamp: undefined
+          },
+          applicationFeePaid: {
+            key: 'applicationFeePaid',
+            available: false,
+            completed: false,
+            editable: false,
+            timestamp: undefined
+          },
+          form2Sent: {
+            key: 'form2Sent',
+            available: false,
+            completed: false,
+            editable: false,
+            timestamp: undefined
+          },
+          verificationDateRecorded: {
+            key: 'verificationDateRecorded',
+            available: false,
+            completed: false,
+            editable: false,
+            timestamp: undefined
+          },
+          certificateIssued: {
+            key: 'certificateIssued',
+            available: false,
+            completed: false,
+            editable: false,
+            timestamp: undefined
+          }
+        }
+
+        return h.response({ tasks }).code(200)
+      } catch (e) {
+        console.log('Error retrieving cdo record:', e)
         throw e
       }
     }
   }
-}]
+]
