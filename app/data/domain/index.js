ad6d69f7fd6f634e9d035b2deea44068bc7a0046
Chris Cole <whitewaterdesign>
2024-06-25 15:45:54 +0100

    feat: 279887 - Domain index

---
+++ app/data/domain/index.js
@@ -0,0 +1,13 @@
+const Cdo = require('./cdo')
+const CdoTaskList = require('./cdoTaskList')
+const Dog = require('./dog')
+const Exemption = require('./exemption')
+const Person = require('./person')
+
+module.exports = {
+  Cdo,
+  CdoTaskList,
+  Dog,
+  Exemption,
+  Person
+}
