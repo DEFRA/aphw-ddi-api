c5e32c2856e48445cfa74e48d7702cd6f04b1a7d
Chris Cole <whitewaterdesign>
2024-06-26 09:42:18 +0100

    feat: 279887 - CdoService dependency injection

---
+++ app/service/config.js
@@ -0,0 +1,22 @@
+
+class ServiceProvider {
+  /**
+   * @type {CdoService}
+   */
+  static cdoService
+
+  /**
+   * @return {CdoService}
+   */
+  static getCdoService () {
+    if (ServiceProvider.cdoService === undefined) {
+      const cdoRepository = require('../repos/cdo')
+      const CdoService = require('./cdo')
+
+      ServiceProvider.cdoService = new CdoService(cdoRepository)
+    }
+    return ServiceProvider.cdoService
+  }
+}
+
+module.exports = ServiceProvider
---
+++ test/unit/app/service/config.test.js
@@ -0,0 +1,21 @@
+const ServiceProvider = require('../../../../app/service/config')
+describe('ServiceProvider', () => {
+  jest.mock('../../../../app/repos/cdo')
+  const cdoRepository = require('../../../../app/repos/cdo')
+
+  test('should instantiate', () => {
+    expect(new ServiceProvider()).toBeInstanceOf(ServiceProvider)
+  })
+
+  test('should initialise cdoService.getCdoService', async () => {
+    const cdoService = ServiceProvider.getCdoService()
+    await cdoService.getTaskList('ED300001')
+    expect(cdoRepository.getCdoTaskList).toHaveBeenCalledTimes(1)
+  })
+
+  test('should use pre-initialised cdoService second time getCdoService is called', async () => {
+    const cdoService = ServiceProvider.getCdoService()
+    await cdoService.getTaskList('ED300002')
+    expect(cdoRepository.getCdoTaskList).toHaveBeenCalledTimes(2)
+  })
+})
