5763411e6602ce9696c473859eea3aa7dfa420a1
Jez Barnsley <114290619+jbarnsley10@users.noreply.github.com>
2024-07-11 17:21:48 +0100

    Disable July deadline (#206)
    * Disable July deadline
    
    * Patch bump
    
    * Rework to satisfy SonarCloud
    
    ---------
    
    Co-authored-by: work <jeremy.barnsley@esynergy.co.uk>

--- app/overnight/expired-neutering-deadline.js
+++ app/overnight/expired-neutering-deadline.js
@@ -6,7 +6,9 @@ const { dbFindAll } = require('../lib/db-functions')
 
 const setExpiredNeuteringDeadlineToInBreach = async (today, user, t) => {
   try {
-    const isAfterJuneDeadline = today >= new Date('2024-07-27')
+    // Rule-part turned off temporarily
+    // const isAfterJuneDeadline = today >= new Date('2024-07-27')
+    const isAfterJuneDeadline = false
     const isAfterDecDeadline = today >= new Date('2025-01-01')
 
     const setStatusToBreach = await dbFindAll(sequelize.models.registration, {
