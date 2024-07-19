05b71350e0533ee2c4ae0b0f4c98210f9214bbfe
Chris Cole <whitewaterdesign>
2024-07-19 10:21:32 +0100

    feat: 408716 - GET /breaches/dog:setBreaches base endpoint

--- app/routes/breaches.js
+++ app/routes/breaches.js
@@ -1,13 +1,27 @@
 const { getBreachCategories } = require('../repos/breaches')
+const ServiceProvider = require('../service/config')
+const { getCallingUser } = require('../auth/get-user')
 
-module.exports = {
-  method: 'GET',
-  path: '/breaches/categories',
-  handler: async (request, h) => {
-    const breachCategories = await getBreachCategories()
+module.exports = [
+  {
+    method: 'GET',
+    path: '/breaches/categories',
+    handler: async (request, h) => {
+      const breachCategories = await getBreachCategories()
 
-    return h.response({
-      breachCategories
-    }).code(200)
+      return h.response({
+        breachCategories
+      }).code(200)
+    }
+  },
+  {
+    method: 'POST',
+    path: '/breaches/dog:setBreaches',
+    handler: async (request, h) => {
+      const payload = request.payload
+      const dogService = ServiceProvider.getDogService()
+      const results = await dogService.setBreaches(payload.indexNumber, payload.dogBreaches, getCallingUser(request))
+      return h.response(results).code(200)
+    }
   }
-}
+]
--- test/integration/narrow/routes/breaches.test.js
+++ test/integration/narrow/routes/breaches.test.js
@@ -1,3 +1,4 @@
+const { devUser } = require('../../../mocks/auth')
 describe('Breaches endpoint', () => {
   const createServer = require('../../../../app/server')
   let server
@@ -5,41 +6,82 @@ describe('Breaches endpoint', () => {
   jest.mock('../../../../app/repos/breaches')
   const { getBreachCategories } = require('../../../../app/repos/breaches')
 
+  jest.mock('../../../../app/service/config')
+  const { getDogService } = require('../../../../app/service/config')
+
+  jest.mock('../../../../app/auth/get-user')
+  const { getCallingUser } = require('../../../../app/auth/get-user')
+
   beforeEach(async () => {
     jest.clearAllMocks()
     server = await createServer()
+    getCallingUser.mockReturnValue(devUser)
     await server.initialize()
   })
 
-  test('GET /breaches/categories route returns 200', async () => {
-    const expectedBreachCategories = [
-      {
-        id: 1,
-        label: 'Dog not covered by third party insurance',
-        short_name: 'NOT_COVERED_BY_INSURANCE'
-      },
-      {
-        id: 2,
-        label: 'Dog not kept on lead or muzzled',
-        short_name: 'NOT_ON_LEAD_OR_MUZZLED'
-      },
-      {
-        id: 3,
-        label: 'Dog kept in insecure place',
-        short_name: 'INSECURE_PLACE'
+  describe('GET /breaches/categories', () => {
+    test('GET /breaches/categories route returns 200', async () => {
+      const expectedBreachCategories = [
+        {
+          id: 1,
+          label: 'Dog not covered by third party insurance',
+          short_name: 'NOT_COVERED_BY_INSURANCE'
+        },
+        {
+          id: 2,
+          label: 'Dog not kept on lead or muzzled',
+          short_name: 'NOT_ON_LEAD_OR_MUZZLED'
+        },
+        {
+          id: 3,
+          label: 'Dog kept in insecure place',
+          short_name: 'INSECURE_PLACE'
+        }
+      ]
+      getBreachCategories.mockResolvedValue(expectedBreachCategories)
+
+      const options = {
+        method: 'GET',
+        url: '/breaches/categories'
       }
-    ]
-    getBreachCategories.mockResolvedValue(expectedBreachCategories)
 
-    const options = {
-      method: 'GET',
-      url: '/breaches/categories'
-    }
+      const response = await server.inject(options)
+      const payload = JSON.parse(response.payload)
+      expect(response.statusCode).toBe(200)
+      expect(payload.breachCategories).toEqual(expectedBreachCategories)
+    })
+  })
 
-    const response = await server.inject(options)
-    const payload = JSON.parse(response.payload)
-    expect(response.statusCode).toBe(200)
-    expect(payload.breachCategories).toEqual(expectedBreachCategories)
+  describe('POST /breaches/dog:setBreaches', () => {
+    test('returns 200', async () => {
+      const setBreachesMock = jest.fn()
+      getDogService.mockReturnValue({
+        setBreaches: setBreachesMock
+      })
+      const options = {
+        method: 'POST',
+        url: '/breaches/dog:setBreaches',
+        payload: {
+          indexNumber: 'ED12345',
+          dogBreaches: [
+            'NOT_COVERED_BY_INSURANCE',
+            'NOT_ON_LEAD_OR_MUZZLED',
+            'INSECURE_PLACE'
+          ]
+        }
+      }
+
+      const response = await server.inject(options)
+      expect(response.statusCode).toBe(200)
+      expect(setBreachesMock).toHaveBeenCalledWith(
+        'ED12345',
+        [
+          'NOT_COVERED_BY_INSURANCE',
+          'NOT_ON_LEAD_OR_MUZZLED',
+          'INSECURE_PLACE'
+        ],
+        devUser)
+    })
   })
 
   afterEach(async () => {
