af4e8d0fe2882b673ad2332f60df948430f8aa66
Chris Cole <whitewaterdesign>
2024-07-29 12:01:25 +0100

    feat: 417819 - Switch off neutering

--- app/overnight/expired-neutering-deadline.js
+++ app/overnight/expired-neutering-deadline.js
@@ -7,8 +7,8 @@ const ServiceProvider = require('../service/config')
 const setExpiredNeuteringDeadlineToInBreach = async (today, user, t) => {
   try {
     // Rule-part turned off temporarily
-    const isAfterJuneDeadline = today >= new Date('2024-07-27')
-    // const isAfterJuneDeadline = false
+    // const isAfterJuneDeadline = today >= new Date('2024-07-27')
+    const isAfterJuneDeadline = false
     const isAfterDecDeadline = today >= new Date('2025-01-01')
 
     const setStatusToBreach = await dbFindAll(sequelize.models.registration, {
