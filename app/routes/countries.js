027f5a5ec2a63c84f8a7a384b0a5f1d156e06c77
Chris Cole <whitewaterdesign>
2024-07-30 11:31:28 +0100

    docs: 419377 - Swagger docs for countries

--- app/routes/countries.js
+++ app/routes/countries.js
@@ -1,9 +1,16 @@
 const { getCountries } = require('../repos/countries')
+const { countriesResponseSchema } = require('../schema/countries')
 
 module.exports = {
   method: 'GET',
   path: '/countries',
-  options: { tags: ['api'] },
+  options: {
+    tags: ['api'],
+    notes: ['Returns a list of countries that are part of the DDI scheme'],
+    response: {
+      schema: countriesResponseSchema
+    }
+  },
   handler: async (request, h) => {
     const countries = await getCountries()
 
