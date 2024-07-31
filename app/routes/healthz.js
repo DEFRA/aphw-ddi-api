89f7dad5f792d5034409c47413581154dbb0af96
Chris Cole <whitewaterdesign>
2024-07-31 19:12:47 +0100

    docs: 419377 - Swagger docs on /health

--- app/routes/healthz.js
+++ app/routes/healthz.js
@@ -1,7 +1,14 @@
+const { okSchema } = require('../schema/common/response/ok')
 module.exports = {
   method: 'GET',
   path: '/healthz',
-  options: { tags: ['api'] },
+  options: {
+    tags: ['api'],
+    response: {
+      schema: okSchema
+    },
+    notes: ['Health endpoint']
+  },
   handler: (request, h) => {
     return h.response('ok').code(200)
   }
--- app/schema/common/response/ok.js
+++ app/schema/common/response/ok.js
@@ -4,6 +4,9 @@ const successResponseSchema = Joi.object({
   result: Joi.string().allow('ok')
 })
 
+const okSchema = Joi.string().allow('ok')
+
 module.exports = {
-  successResponseSchema
+  successResponseSchema,
+  okSchema
 }
