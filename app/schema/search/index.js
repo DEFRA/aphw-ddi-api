d2241b84a87e1a692342637491c394d8a3fdf759
Jez Barnsley <114290619+jbarnsley10@users.noreply.github.com>
2024-08-06 10:14:34 +0100

    422586 auto update (#229)
    * Temp
    
    * Auto-updates
    Added extra coverage
    
    * Patch bump
    
    * Tweaked after SonarCloud suggestions
    
    * Further change as per SonarCloud
    
    ---------
    
    Co-authored-by: work <jeremy.barnsley@esynergy.co.uk>

--- app/schema/search/index.js
+++ app/schema/search/index.js
@@ -6,26 +6,26 @@ const searchQueryParamsSchema = Joi.object({
 })
 
 const searchResponseAddress = Joi.object({
-  address_line_1: Joi.string().optional().allow(''),
-  address_line_2: Joi.string().optional().allow(''),
-  town: Joi.string().optional().allow(''),
-  postcode: Joi.string().optional().allow('')
+  address_line_1: Joi.string().optional().allow('').allow(null),
+  address_line_2: Joi.string().optional().allow('').allow(null),
+  town: Joi.string().optional().allow('').allow(null),
+  postcode: Joi.string().optional().allow('').allow(null)
 }).optional()
 
 const searchResponseSchema = Joi.object({
   results: Joi.array().items({
-    email: Joi.string().optional().allow(''),
+    email: Joi.string().optional().allow('').allow(null),
     address: searchResponseAddress,
-    dogIndex: Joi.string().optional().allow(''),
-    dogName: Joi.string().optional().allow(''),
-    dogStatus: Joi.string().optional(),
+    dogIndex: Joi.string().optional().allow('').allow(null),
+    dogName: Joi.string().optional().allow('').allow(null),
+    dogStatus: Joi.string().optional().allow(null),
     personReference: Joi.string().optional(),
-    firstName: Joi.string().optional().allow(''),
-    lastName: Joi.string().optional().allow(''),
-    microchipNumber: Joi.string().optional().allow(''),
-    microchipNumber2: Joi.string().optional().allow(''),
-    organisationName: Joi.string().optional().allow(''),
-    dogId: Joi.number().optional(),
+    firstName: Joi.string().optional().allow('').allow(null),
+    lastName: Joi.string().optional().allow('').allow(null),
+    microchipNumber: Joi.string().optional().allow('').allow(null),
+    microchipNumber2: Joi.string().optional().allow('').allow(null),
+    organisationName: Joi.string().optional().allow('').allow(null),
+    dogId: Joi.number().optional().allow(null),
     personId: Joi.number().optional(),
     distance: Joi.number().optional(),
     rank: Joi.number().optional()
