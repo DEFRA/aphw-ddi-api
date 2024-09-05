87a9e2d62945f88b4805fc19f1d8458136120d13
work <jeremy.barnsley@esynergy.co.uk>
2024-09-05 16:47:50 +0100

    Fixed purge order

--- app/repos/people.js
+++ app/repos/people.js
@@ -550,10 +550,10 @@ const purgePersonByReferenceNumber = async (reference, user, transaction) => {
     await personContact.contact.destroy({ force: true, transaction })
   }
 
-  await person.destroy({ force: true, transaction })
-
   await sequelize.models.search_match_code.destroy({ where: { person_id: person.id }, force: true, transaction })
   await sequelize.models.search_tgram.destroy({ where: { person_id: person.id }, force: true, transaction })
+
+  await person.destroy({ force: true, transaction })
 
   await sendPermanentDeleteToAudit(PERSON, person, user)
 }
