7aa2a9fbfa7aec03e47a036552084b82545c105a
Chris Cole <56303993+whitewaterdesign@users.noreply.github.com>
2024-09-20 11:11:14 +0100

    feat: 430523 - Remove logging (#250)
    * feat: 430523 - Remove logging
    
    * chore: patch v0.75.6
    
    ---------
    
    Co-authored-by: Chris Cole <whitewaterdesign>

--- app/auth/token-validator.js
+++ app/auth/token-validator.js
@@ -26,26 +26,20 @@ const returnVal = (
 
 const checkTokenOnline = async (username, token) => {
   try {
-    console.log('~~~~~~ Chris Debug ~~~~~~ checking OIDC', '')
     const payload = await getUserInfo(token)
-    console.log('~~~~~~ Chris Debug ~~~~~~ OIDC payload', 'Payload', payload)
 
     return payload && payload.email === username && payload.email_verified === true
   } catch (e) {
-    console.log('~~~~~~ Chris Debug ~~~~~~ failed OIDC check', '')
     return false
   }
 }
 
 const validatePortal = (_username, payload) => {
-  console.log('~~~~~~ Chris Debug ~~~~~~ validating portal request', '')
   return returnVal(true, payload)
 }
 const validateEnforcement = async (username, payload) => {
-  console.log('~~~~~~ Chris Debug ~~~~~~ validating enforcement request', '')
   const { token } = payload
 
-  console.log('~~~~~~ Chris Debug ~~~~~~ ', 'Token', token)
   if (!token) {
     return returnVal(false)
   }
@@ -53,10 +47,8 @@ const validateEnforcement = async (username, payload) => {
   const now = new Date()
   const hash = crypto.createHash('sha512').update(token).digest('hex')
 
-  console.log('~~~~~~ Chris Debug ~~~~~~ ', 'Hash', hash)
   const cached = hashCache.get(username)
   if (cached) {
-    console.log('~~~~~~ Chris Debug ~~~~~~ using cached token for Enforcement', '')
     if (cached.expiry > now && cached.hash === hash) {
       // Valid non-expired token
       // console.info(`Got from cache - expiry in ${Math.trunc((cached.expiry - now) / 1000 / 60)} mins`)
@@ -67,11 +59,9 @@ const validateEnforcement = async (username, payload) => {
   // Validate token contents and store in cache
   const validToken = await checkTokenOnline(username, token)
   if (validToken) {
-    console.log('~~~~~~ Chris Debug ~~~~~~ enforcement token valid', '')
     const enabled = await isAccountEnabled(username)
-    console.log('~~~~~~ Chris Debug ~~~~~~ enforcement user is enabled', 'Enabled', enabled)
+
     if (enabled) {
-      console.log('~~~~~~ Chris Debug ~~~~~~ enforcement setting hash', '')
       hashCache.set(username, { hash, expiry: addMinutes(now, expiryPeriodInMins) })
       return returnVal(true, payload)
     }
@@ -81,7 +71,6 @@ const validateEnforcement = async (username, payload) => {
 }
 
 const validate = async (artifacts, _request, _h) => {
-  console.log('~~~~~~ Chris Debug ~~~~~~ validating JWT token', 'Artifacts', artifacts)
   const decoded = artifacts.decoded
   const payload = decoded.payload
   const username = payload.username
