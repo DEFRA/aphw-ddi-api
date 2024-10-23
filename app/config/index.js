aba4182b1a09438620c5b70bfeb39767fe73fdff
work <jeremy.barnsley@esynergy.co.uk>
2024-10-23 16:07:31 +0100

    Removed debug

--- app/config/index.js
+++ app/config/index.js
@@ -60,7 +60,7 @@ const result = schema.validate(config, {
 
 // Throw if config is invalid
 if (result.error) {
-  throw new Error(`The server config is invalid. ${result.error.message} ${JSON.stringify(config)}`)
+  throw new Error(`The server config is invalid. ${result.error.message}`)
 }
 
 // Use the joi validated value
