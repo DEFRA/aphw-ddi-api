d5b402072179bc65dd2779a4e5bd6256347a9ced
Chris Cole <56303993+whitewaterdesign@users.noreply.github.com>
2024-10-31 21:19:05 +0000

    bug: 466432 - Report Type Auditing Duplicates (#283)
    * bug: 466432 - Report Type Auditing Duplicates
    
    * chore: patch v0.79.14
    
    * fix: Activity text
    
    ---------
    
    Co-authored-by: Chris Cole <whitewaterdesign>

--- app/lib/email-helper.js
+++ app/lib/email-helper.js
@@ -1,8 +1,9 @@
 const config = require('../config/index')
-const { emailTypes, reportSomethingSubjectLines, reportSomethingAudit } = require('../constants/email-types')
+const { emailTypes, reportSomethingSubjectLines, reportSomethingAudit, reportTypes } = require('../constants/email-types')
 const { lookupPoliceForceByEmail } = require('../repos/police-forces')
 const { sendEmail } = require('../messaging/send-email')
 const { sendActivityToAudit } = require('../messaging/send-audit')
+const { v4: uuidv4 } = require('uuid')
 
 const sendReportSomethingEmails = async (payload) => {
   const userField = payload?.fields.find(x => x.name === 'ReportedBy')
@@ -43,31 +44,62 @@ const sendReportSomethingEmails = async (payload) => {
   }
 }
 
-const createAuditPayload = (data, pk, source, targetPk) => {
+const createAuditPayload = (data, pk, source, targetPk, activityId, reportType) => {
   return {
+    activityId,
     activity: reportSomethingAudit.id,
     activityType: reportSomethingAudit.activityType,
     pk,
     source,
     activityDate: new Date(),
     targetPk,
-    activityLabel: `${reportSomethingAudit.label} ${reportSomethingAudit.activityType} from ${data?.policeForce}`
+    reportType,
+    activityLabel: `${reportSomethingAudit.label} from ${data?.policeForce}`
   }
 }
-
+/**
+ * @typedef ReportData
+ * @property {string} sourceType - e.g. 'dog',
+ * @property {string} personReference - e.g. 'P-BBDC-8579',
+ * @property {string} pk - e.g. 'ED300002',
+ * @property {string[]} dogs - e.g. [ 'ED300002' ],
+ * @property {ReportType} reportType - e.g. 'in-breach',
+ * @property {string} subTitle - e.g. 'Dog ED300002',
+ * @property {{ indexNumber: string }} dogChosen - e.g. { indexNumber: 'ED300002' },
+ * @property {string[]} dogBreaches - e.g. [ 'Dog not kept on lead or muzzled in public place' ]
+ */
+/**
+ * @typedef ReportDataData
+ * @property {ReportData} reportData
+ * @property {string} policeForce
+ * @property {string} username
+ */
+/**
+ * @param {ReportDataData} data
+ * @return {Promise<void>}
+ */
 const createAuditsForReportSomething = async (data) => {
   if (!data?.reportData) {
     return
   }
 
-  for (const dogIndex of data.reportData?.dogs ?? []) {
-    const payload = createAuditPayload(data, dogIndex, 'dog', 'dog')
-    await sendActivityToAudit(payload, { username: data?.username, displayname: data?.username })
+  const reportToOwner = [reportTypes.changedAddress, reportTypes.somethingElse].includes(data.reportData.reportType)
+  const reportId = uuidv4()
+  const reportType = data.reportData.reportType
+  let dogs = data.reportData.dogs ?? []
+
+  if (data.reportData.dogChosen) {
+    dogs = [data.reportData.dogChosen.indexNumber]
   }
 
-  if (data.reportData.sourceType === 'owner') {
-    const payload = createAuditPayload(data, data.reportData.pk, 'owner', 'owner')
-    await sendActivityToAudit(payload, { username: data?.username, displayname: data?.username })
+  for (const dogIndex of dogs) {
+    const payload = createAuditPayload(data, dogIndex, 'dog', 'dog', reportId, reportType)
+    await sendActivityToAudit(payload, { username: data.username, displayname: data.username })
+  }
+
+  if (reportToOwner) {
+    const payload = createAuditPayload(data, data.reportData.personReference, 'owner', 'owner', reportId, reportType)
+    await sendActivityToAudit(payload, { username: data.username, displayname: data.username })
   }
 }
 
