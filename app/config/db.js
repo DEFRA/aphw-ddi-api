9adec419596d16483ea21509d2eb105787f4d6f5
Jez Barnsley <114290619+jbarnsley10@users.noreply.github.com>
2024-11-13 07:56:29 +0000

    Add new breach reason (#295)
    * Add new breach reason
    
    * Patch bump
    
    * Added db config test
    
    * Removed test ignore
    
    * Extra coverage
    
    * Fixed test
    
    * Fixed test result
    
    ---------
    
    Co-authored-by: work <jeremy.barnsley@esynergy.co.uk>

--- app/config/db.js
+++ app/config/db.js
@@ -1,5 +1,3 @@
-/* istanbul ignore file */
-
 const { Sequelize } = require('sequelize')
 const { DefaultAzureCredential } = require('@azure/identity')
 
@@ -7,7 +5,7 @@ const isProd = () => {
   return process.env.NODE_ENV === 'production'
 }
 
-const MAX_CONNECTIONS = 50
+const MAX_CONNECTIONS = 20
 
 const hooks = {
   beforeConnect: async (cfg) => {
