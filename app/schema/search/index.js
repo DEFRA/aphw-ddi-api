be11e8174f48f85920586b955e737d92374a883e
work <jeremy.barnsley@esynergy.co.uk>
2024-11-20 15:52:43 +0000

    Search design change

--- app/schema/search/index.js
+++ app/schema/search/index.js
@@ -5,13 +5,6 @@ const searchQueryParamsSchema = Joi.object({
   terms: Joi.string().required()
 })
 
-const dogAddress = Joi.object({
-  address_line_1: Joi.string().optional().allow('').allow(null),
-  address_line_2: Joi.string().optional().allow('').allow(null),
-  town: Joi.string().optional().allow('').allow(null),
-  postcode: Joi.string().optional().allow('').allow(null)
-}).optional()
-
 const ownerAddress = Joi.string().optional().allow('').allow(null)
 
 const searchResponseSchema = Joi.object({
@@ -19,7 +12,7 @@ const searchResponseSchema = Joi.object({
     results: Joi.array().items({
       searchType: Joi.string(),
       email: Joi.string().optional().allow('').allow(null),
-      address: Joi.when('searchType', { is: 'dog', then: dogAddress, otherwise: ownerAddress }),
+      address: ownerAddress,
       dogIndex: Joi.string().optional().allow('').allow(null),
       dogName: Joi.string().optional().allow('').allow(null),
       dogStatus: Joi.string().optional().allow(null),
