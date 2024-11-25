93bf57d0e8c26d4230230362b2c5271711f2191e
work <jeremy.barnsley@esynergy.co.uk>
2024-11-25 15:38:14 +0000

    Temp

---
+++ app/data/models/police-force-group.js
@@ -0,0 +1,49 @@
+module.exports = (sequelize, DataTypes) => {
+  const policeForceGroup = sequelize.define('police_force_group', {
+    id: {
+      autoIncrement: true,
+      autoIncrementIdentity: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    display_text: {
+      type: DataTypes.STRING(200),
+      allowNull: false
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.fn('now')
+    },
+    deleted_at: {
+      type: DataTypes.DATE,
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    tableName: 'police_force_group',
+    paranoid: true,
+    createdAt: 'created_at',
+    deletedAt: 'deleted_at',
+    updatedAt: 'updated_at',
+    indexes: [
+      {
+        name: 'police_force_group_pkey',
+        unique: true,
+        fields: [
+          { name: 'id' }
+        ]
+      }
+    ]
+  })
+
+  policeForceGroup.associate = models => {
+    policeForceGroup.belongsTo(models.force_group_item, {
+      as: 'force_group_item',
+      foreignKey: 'force_group_item_id'
+    })
+  }
+
+  return policeForceGroup
+}
