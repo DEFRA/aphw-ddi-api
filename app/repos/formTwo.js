52e0ca814b15ad29912e25796c5caf2c34f11f25
Chris Cole <whitewaterdesign>
2024-12-07 21:08:37 +0000

    test: 479627 - Submit Form Two repo

---
+++ app/repos/formTwo.js
@@ -0,0 +1,77 @@
+const sequelize = require('../config/db')
+const { Op } = require('sequelize')
+const { DuplicateResourceError } = require('../errors/duplicate-record')
+const { NotFoundError } = require('../errors/not-found')
+const { createAuditsForFormTwo } = require('../lib/email-helper')
+/**
+ *
+ * @param {string} indexNumber
+ * @param {CdoTaskList} cdoTaskList
+ * @param {{
+ *     microchipVerification: string;
+ *     neuteringConfirmation: string;
+ *     microchipNumber: string;
+ *     microchipDeadline: string;
+ *     dogNotNeutered: boolean;
+ *     dogNotFitForMicrochip: boolean;
+ * }} payload
+ * @param {string} username
+ * @param {() => any} callbackFn
+ * @param [transaction]
+ */
+const submitFormTwo = async (indexNumber, cdoTaskList, payload, { username }, callbackFn, transaction) => {
+  if (!transaction) {
+    return await sequelize.transaction(async (t) => submitFormTwo(indexNumber, cdoTaskList, payload, { username }, callbackFn, t))
+  }
+
+  const dogId = parseInt(indexNumber.replace('ED', ''))
+
+  const formTwo = await sequelize.models.form_two.findOne({
+    where: { dog_id: dogId },
+    transaction
+  })
+
+  if (formTwo) {
+    throw new DuplicateResourceError('Form Two already submitted')
+  }
+
+  const registration = await sequelize.models.registration.findOne({
+    where: { dog_id: dogId },
+    transaction
+  })
+  if (!registration) {
+    throw new NotFoundError('Cdo not found')
+  }
+
+  const userAccount = await sequelize.models.user_account.findOne({
+    where: { username },
+    transaction
+  })
+  if (!userAccount) {
+    throw new NotFoundError('User account not found')
+  }
+
+  await sequelize.models.form_two.create({
+    dog_id: dogId,
+    registration_id: registration.id,
+    form_two_submitted: new Date(),
+    submitted_by_id: userAccount.id
+  }, { transaction })
+
+  await createAuditsForFormTwo({
+    indexNumber,
+    microchipVerification: payload.microchipVerification,
+    neuteringConfirmation: payload.neuteringConfirmation,
+    microchipNumber: payload.microchipNumber,
+    microchipDeadline: payload.microchipDeadline,
+    policeForce: userAccount.police_force?.name,
+    dogNotNeutered: payload.dogNotNeutered,
+    dogNotFitForMicrochip: payload.dogNotFitForMicrochip,
+    username
+  })
+  await callbackFn()
+}
+
+module.exports = {
+  submitFormTwo
+}
---
+++ test/unit/app/repos/formTwo.test.js
@@ -0,0 +1,136 @@
+const { buildCdoTaskList, buildCdoDog } = require('../../../mocks/cdo/domain')
+const { DuplicateResourceError } = require('../../../../app/errors/duplicate-record')
+const { buildRegistrationDao } = require('../../../mocks/cdo/get')
+const { buildUserAccount } = require('../../../mocks/user-accounts')
+const { NotFoundError } = require('../../../../app/errors/not-found')
+
+describe('formTwo', () => {
+  jest.mock('../../../../app/config/db', () => ({
+    transaction: jest.fn(),
+    col: jest.fn(),
+    models: {
+      form_two: {
+        findOne: jest.fn(),
+        create: jest.fn()
+      },
+      registration: {
+        findOne: jest.fn()
+      },
+      user_account: {
+        findOne: jest.fn()
+      }
+    },
+    fn: jest.fn(),
+    literal: jest.fn()
+  }))
+
+  const sequelize = require('../../../../app/config/db')
+  const callback = jest.fn()
+
+  jest.mock('../../../../app/lib/email-helper')
+  const { createAuditsForFormTwo } = require('../../../../app/lib/email-helper')
+
+  const { submitFormTwo } = require('../../../../app/repos/formTwo')
+
+  const middleEarthUser = {
+    username: 'bilbo.baggins@shire.police.me',
+    displayname: 'Bilbo Baggins'
+  }
+  const cdoTaskList = buildCdoTaskList({
+    dog: buildCdoDog({
+      indexNumber: 'ED300100',
+      microchipNumber: '223456789012345',
+      name: 'Pip'
+    })
+  })
+
+  const defaultPayload = {
+    microchipVerification: '03/12/2024',
+    neuteringConfirmation: '04/12/2024',
+    microchipNumber: '543210987654321',
+    microchipDeadline: '',
+    dogNotNeutered: false,
+    dogNotFitForMicrochip: false
+  }
+  describe('submitFormTwo', () => {
+    beforeEach(() => {
+      sequelize.models.registration.findOne.mockResolvedValue(buildRegistrationDao({
+        id: 97
+      }))
+      sequelize.models.user_account.findOne.mockResolvedValue(buildUserAccount({
+        id: 5,
+        username: 'bilbo.baggins@shire.police.me',
+        police_force: {
+          id: 2,
+          name: 'Shire Citizens Constabulary'
+        }
+      }))
+    })
+    afterEach(() => {
+      jest.resetAllMocks()
+    })
+
+    test('should start new transaction if none passed', async () => {
+      sequelize.transaction.mockImplementation(callbackFn => callbackFn({}))
+
+      await submitFormTwo('ED300100', cdoTaskList, defaultPayload, middleEarthUser, callback)
+
+      expect(sequelize.transaction).toHaveBeenCalledTimes(1)
+    })
+
+    test('should submit a form two', async () => {
+      /**
+       * @type {FormTwoAuditDetails}
+       */
+      const expectedAudit = {
+        indexNumber: 'ED300100',
+        microchipVerification: '03/12/2024',
+        neuteringConfirmation: '04/12/2024',
+        microchipDeadline: '',
+        microchipNumber: '543210987654321',
+        dogNotNeutered: false,
+        dogNotFitForMicrochip: false,
+        policeForce: 'Shire Citizens Constabulary',
+        username: 'bilbo.baggins@shire.police.me'
+      }
+
+      await submitFormTwo('ED300100', cdoTaskList, defaultPayload, middleEarthUser, callback, {})
+
+      expect(callback).toHaveBeenCalled()
+      expect(sequelize.models.form_two.findOne).toHaveBeenCalledWith({
+        where: { dog_id: 300100 },
+        transaction: {}
+      })
+      expect(sequelize.models.registration.findOne).toHaveBeenCalledWith({
+        where: { dog_id: 300100 },
+        transaction: {}
+      })
+      expect(sequelize.models.user_account.findOne).toHaveBeenCalledWith({
+        where: { username: 'bilbo.baggins@shire.police.me' },
+        transaction: {}
+      })
+      expect(sequelize.models.form_two.create).toHaveBeenCalledWith({
+        dog_id: 300100,
+        registration_id: 97,
+        submitted_by_id: 5,
+        form_two_submitted: expect.any(Date)
+      }, { transaction: {} })
+      expect(createAuditsForFormTwo).toHaveBeenCalledWith(expectedAudit)
+    })
+
+    test('should throw if a form two already exists', async () => {
+      sequelize.models.form_two.findOne.mockResolvedValue({
+        id: 1
+      })
+      await expect(submitFormTwo('ED300100', cdoTaskList, defaultPayload, middleEarthUser, callback, {})).rejects.toThrow(new DuplicateResourceError('Form Two already submitted'))
+    })
+    test('should throw if registration is not found', async () => {
+      sequelize.models.registration.findOne.mockResolvedValue(null)
+      await expect(submitFormTwo('ED300100', cdoTaskList, defaultPayload, middleEarthUser, callback, {})).rejects.toThrow(new NotFoundError('Cdo not found'))
+    })
+    test('should throw if registration is not found', async () => {
+      sequelize.models.user_account.findOne.mockResolvedValue(null)
+      await expect(submitFormTwo('ED300100', cdoTaskList, defaultPayload, middleEarthUser, callback, {})).rejects.toThrow(new NotFoundError('User account not found'))
+    })
+  })
+})
