ffcb0c268bfba8ce38cd50b0f2ef1afa2c5de435
Jez Barnsley <114290619+jbarnsley10@users.noreply.github.com>
2025-01-17 14:53:20 +0000

    Shuffles address (#355)
    * Shuffles address
    
    * Patch bump
    
    ---------
    
    Co-authored-by: work <jeremy.barnsley@esynergy.co.uk>

--- app/lib/address-helper.js
+++ app/lib/address-helper.js
@@ -40,8 +40,39 @@ const buildAddressForSearchResults = (address) => {
   return buildAddressString(address)
 }
 
+const shuffleAddress = (address) => {
+  const addrParts = []
+  if (address?.addressLine1 && address?.addressLine1 !== '') {
+    addrParts.push(address.addressLine1)
+  }
+  if (address?.addressLine2 && address?.addressLine2 !== '') {
+    addrParts.push(address.addressLine2)
+  }
+  if (address?.town && address?.town !== '') {
+    addrParts.push(address.town)
+  }
+  if (address?.postcode && address?.postcode !== '') {
+    addrParts.push(address.postcode)
+  }
+
+  if (addrParts.length < 4) {
+    const blankRowsRequired = 4 - addrParts.length
+    for (let i = 0; i < blankRowsRequired; i++) {
+      addrParts.push('')
+    }
+  }
+
+  return {
+    addressLine1: addrParts[0],
+    addressLine2: addrParts[1],
+    town: addrParts[2],
+    postcode: addrParts[3]
+  }
+}
+
 module.exports = {
   buildAddressString,
   buildAddressStringAlternate,
-  buildAddressForSearchResults
+  buildAddressForSearchResults,
+  shuffleAddress
 }
--- test/unit/app/lib/address-helper.test.js
+++ test/unit/app/lib/address-helper.test.js
@@ -1,4 +1,4 @@
-const { buildAddressString, buildAddressStringAlternate } = require('../../../../app/lib/address-helper')
+const { buildAddressString, buildAddressStringAlternate, shuffleAddress } = require('../../../../app/lib/address-helper')
 
 describe('AddressHelper test', () => {
   describe('buildAddressString test', () => {
@@ -44,6 +44,48 @@ describe('AddressHelper test', () => {
         town: null,
         postcode: 'postcode'
       })).toEqual('addr1, postcode')
+    })
+  })
+
+  describe('shuffleAddress test', () => {
+    test('should build address with all parts', () => {
+      expect(shuffleAddress({
+        addressLine1: 'addr1',
+        addressLine2: 'addr2',
+        town: 'town',
+        postcode: 'postcode'
+      })).toEqual({
+        addressLine1: 'addr1',
+        addressLine2: 'addr2',
+        town: 'town',
+        postcode: 'postcode'
+      })
+    })
+    test('should build address when missing 1 line', () => {
+      expect(shuffleAddress({
+        addressLine1: 'addr1',
+        addressLine2: '',
+        town: 'town',
+        postcode: 'postcode'
+      })).toEqual({
+        addressLine1: 'addr1',
+        addressLine2: 'town',
+        town: 'postcode',
+        postcode: ''
+      })
+    })
+    test('should build address when missing 2 lines', () => {
+      expect(shuffleAddress({
+        addressLine1: 'addr1',
+        addressLine2: '',
+        town: '',
+        postcode: 'postcode'
+      })).toEqual({
+        addressLine1: 'addr1',
+        addressLine2: 'postcode',
+        town: '',
+        postcode: ''
+      })
     })
   })
 })
